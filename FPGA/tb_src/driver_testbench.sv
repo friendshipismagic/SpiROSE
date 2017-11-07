efb671b35b59150c5910ec463afc1f31d3ae9581
adrien <adrien.marcenat@telecom-paristech.fr>
2017-11-07 22:36:52 +0100

    FPGA/tb_src/driver_testbench.sv: dummy testbench
    Add empty tesbench for the driver module.

---
+++ FPGA/tb_src/driver_testbench.sv
@@ -0,0 +1,23 @@
+module driver_testbench #(parameter DATA_WIDTH = 9);
+
+logic clk, nrst, lat;
+logic [DATA_WIDTH -1:0] voxel_in, voxel_out;
+
+driver #(.DATA_WIDTH(DATA_WIDTH)) mce (.clk(clk),
+                                       .nrst(nrst),
+                                       .voxel_in(voxel_in),
+                                       .voxel_out(voxel_out),
+                                       .lat(lat));
+
+initial
+begin
+    logic [DATA_WIDTH - 1:0] voxel;
+
+    repeat(1000)
+    begin
+    $display("Everything went fine");
+    $finish;
+end
+
+endmodule
+
