17aa3e01d17b1f9c23498df91750fe9d304808cf
Vincent Charbonnieras <vincent.charbonnieras@telecom-paristech.fr>
2017-11-08 21:52:43 +0100

    Warnings for ST HAL Drivers and ugfx library removed, user source file warnings treated as errors
    Add rfc1149/rose-dev image for lint step as this step needs clang format.

--- LCD_Display/main.c
+++ LCD_Display/main.c
@@ -1,7 +1,7 @@
-#include "gfx.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "gfx.h"
 
 #define WIDTH gdispGetWidth()
 #define HEIGHT gdispGetHeight()
@@ -29,36 +29,27 @@ static const GWidgetStyle SpiROSEStyle = {
     HTML2COLOR(0x3333ff),
 
     // enabled widget color set
-    {
-        // Colour of the text part
-        HTML2COLOR(0x444444),
-        // Colour of the edges
-        HTML2COLOR(0x404040),
-        // Main background colour
-        HTML2COLOR(0xff0000),
-        // Colour for the used part of sliders
-        HTML2COLOR(0x990000)
-    },
+    {// Colour of the text part
+     HTML2COLOR(0x444444),
+     // Colour of the edges
+     HTML2COLOR(0x404040),
+     // Main background colour
+     HTML2COLOR(0xff0000),
+     // Colour for the used part of sliders
+     HTML2COLOR(0x990000)},
 
     // disabled widget color set, same structure as above
-    {
-        HTML2COLOR(0x444444),
-        HTML2COLOR(0x808080),
-        HTML2COLOR(0xE0E0E0),
-        HTML2COLOR(0xC0E0C0)
-    },
+    {HTML2COLOR(0x444444), HTML2COLOR(0x808080), HTML2COLOR(0xE0E0E0),
+     HTML2COLOR(0xC0E0C0)},
 
     // pressed widget color set, same structure as above
     {
-        HTML2COLOR(0xFF0000),
-        HTML2COLOR(0x404040),
-        HTML2COLOR(0x808080),
+        HTML2COLOR(0xFF0000), HTML2COLOR(0x404040), HTML2COLOR(0x808080),
         HTML2COLOR(0x00E000),
-    }
-};
+    }};
 
 int main(void) {
-    GEvent*    pe;
+    GEvent *pe;
 
     // Initialization of the display
     gfxInit();
@@ -75,41 +66,41 @@ int main(void) {
     widgetCreation();
 
     // Slider's position
-    int pos=0;
+    int pos = 0;
 
-    while(TRUE) {
+    while (TRUE) {
         pe = geventEventWait(&gl, TIME_INFINITE);
-        
-        if (pe->type == GEVENT_GWIN_SLIDER && ((GEventGWinSlider *)pe)->action == GSLIDER_EVENT_SET){
+
+        if (pe->type == GEVENT_GWIN_SLIDER &&
+            ((GEventGWinSlider *)pe)->action == GSLIDER_EVENT_SET) {
             pos = gwinSliderGetPosition(slider);
             char str[12];
-            itoa(pos*3.6, str, 10);
+            itoa(pos * 3.6, str, 10);
             gwinSetText(slider, str, TRUE);
             // TODO: Send rotation information to the SBC
+        } else if (pe->type == GEVENT_GWIN_BUTTON &&
+                   ((GEventGWinButton *)pe)->gwin == gButton1) {
+            // TODO : Send start command to SBC
+        } else if (pe->type == GEVENT_GWIN_BUTTON &&
+                   ((GEventGWinButton *)pe)->gwin == gButton2) {
+            // TODO : Send stop command to SBC
         }
-        else if (pe->type == GEVENT_GWIN_BUTTON && ((GEventGWinButton *)pe)->gwin == gButton1){
-            //TODO : Send start command to SBC
-        }
-        else if (pe->type == GEVENT_GWIN_BUTTON && ((GEventGWinButton *)pe)->gwin == gButton2){
-            //TODO : Send stop command to SBC
-        }
-
-        
-    }   
+    }
 
     return 0;
 }
 
-static void widgetCreation(void){
+static void widgetCreation(void) {
     // Structure used for widget initialisation
-    GWidgetInit    wi;
+    GWidgetInit wi;
     gwinWidgetClearInit(&wi);
 
     // Structure used for window initialisation
     GWindowInit gwi;
     gwinClearInit(&gwi);
 
-    // Opens an image, need the name of the image defined in the image_name.h header file
+    // Opens an image, need the name of the image defined in the image_name.h
+    // header file
     gdispImageOpenFile(&myImage, "degrade_bleu.png");
 
     /* Container */
@@ -123,13 +114,11 @@ static void widgetCreation(void){
     // This special rendering function allows an opened image to be displayed
     wi.customDraw = gwinContainerDraw_Image;
 
-    //This parameter is passed to the rendering function above
-    wi.customParam = (void*)&myImage;
+    // This parameter is passed to the rendering function above
+    wi.customParam = (void *)&myImage;
 
     // Widget creation according to GWidgetInit structure
     mainContainer = gwinContainerCreate(0, &wi, GWIN_CONTAINER_BORDER);
-
-
 
     /* Button1 */
     wi.g.width = 160;
@@ -140,12 +129,12 @@ static void widgetCreation(void){
     wi.customDraw = NULL;
     wi.g.parent = mainContainer;
     gButton1 = gwinButtonCreate(0, &wi);
-    
+
     /* Button2 */
     wi.g.width = 160;
     wi.g.height = 30;
     wi.g.y = 10;
-    wi.g.x = WIDTH-10-160;
+    wi.g.x = WIDTH - 10 - 160;
     wi.text = "Stop SpiROSE";
     wi.g.parent = mainContainer;
     gButton2 = gwinButtonCreate(0, &wi);
@@ -158,12 +147,12 @@ static void widgetCreation(void){
     wi.text = "ROTATION";
     wi.g.parent = mainContainer;
     slider = gwinSliderCreate(0, &wi);
-    
+
     /* Console */
     gwi.show = TRUE;
     gwi.x = 10;
     gwi.y = 50;
-    gwi.width = WIDTH-20;
+    gwi.width = WIDTH - 20;
     gwi.height = 130;
     gwi.parent = mainContainer;
     console = gwinConsoleCreate(0, &gwi);
@@ -176,10 +165,11 @@ static void widgetCreation(void){
     gwinClear(console);
 
     // Print any data to the console
-    gwinPrintf(console, "SpiROSE Status : WIP\nIMU Information:\nX acceleration:\nY acceleration:\nZ acceleration:\n");
-    
+    gwinPrintf(console,
+               "SpiROSE Status : WIP\nIMU Information:\nX acceleration:\nY "
+               "acceleration:\nZ acceleration:\n");
+
     // We want to listen for widget events
     geventListenerInit(&gl);
     gwinAttachListener(&gl);
 }
-
