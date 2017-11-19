ac390678c903b13ec74b2b4d496d6633b7679acf
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2017-11-19 19:28:45 +0100

    FPGA/systemc/driver.hpp: add WRTGS naive handling

--- FPGA/src/systemc/driver.hpp
+++ FPGA/src/systemc/driver.hpp
@@ -5,6 +5,8 @@
 class Driver : public sc_module {
     SC_HAS_PROCESS(Driver);
 
+    enum driver_mode_t { MODE_DATA, MODE_FCWRT };
+
     public:
     inline Driver(const sc_module_name& name)
         : sc_module(name), gclk("gclk"), sclk("sclk"), sin("sin"), lat("lat") {
@@ -15,7 +17,7 @@ class Driver : public sc_module {
     void handle_sin() {
         while (true) {
             wait();
-            gs_reg = ((gs_reg.read() << 1) & ~1) | sin;
+            shift_reg = ((shift_reg.read() << 1) & ~1) | sin;
         }
     }
 
@@ -25,9 +27,24 @@ class Driver : public sc_module {
             if (lat) {
                 lat_counter = lat_counter.read() + 1;
             } else if (lat_counter == 1) {
-                // WRTGS
+                // WRTGS, write to GS data at the GS counter position
+
+                // Calculate the slice where to write in GS
+                int end = 48 * (1 + gs_counter.read()) - 1;
+
+                // We need to extract the data from signal to apply range
+                // operator
+                auto vec = gs1_data.read();
+                vec(end, end - 47) = shift_reg.read()(47, 0);
+
+                gs1_data.write(vec);
             } else if (lat_counter == 3) {
-                // LATGS
+                // LATGS, write to GS data and latch GS1 to GS2
+            } else if (lat_counter == 5) {
+                // WRTFC
+            } else if (lat_counter == 15) {
+                // FCWRTEN, enable to write to FC data latch
+                current_mode = MODE_FCWRT;
             }
         }
     }
@@ -40,6 +57,12 @@ class Driver : public sc_module {
     sc_in<bool> lat;
 
     private:
-    sc_signal<sc_bv<48>> gs_reg;
+    sc_signal<sc_bv<48>> shift_reg;
+    sc_signal<sc_bv<48>> fc_data;
+    sc_signal<sc_bv<768>> gs1_data;
+    sc_signal<sc_bv<768>> gs2_data;
+
     sc_signal<int> lat_counter;
+    sc_signal<int> gs_counter;
+    sc_signal<driver_mode_t> current_mode;
 };
