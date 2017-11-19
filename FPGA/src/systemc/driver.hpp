8f215519a1acbfb9c0aa3c30a2ce1682de01c462
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2017-11-19 18:13:22 +0100

    FPGA/systemc/driver.hpp: add handle_{sin/lat} process

--- FPGA/src/systemc/driver.hpp
+++ FPGA/src/systemc/driver.hpp
@@ -6,11 +6,40 @@ class Driver : public sc_module {
     SC_HAS_PROCESS(Driver);
 
     public:
-    inline Driver(const sc_module_name& name) : sc_module(name) {
-        SC_CTHREAD(handle_sin, clk);
+    inline Driver(const sc_module_name& name)
+        : sc_module(name), gclk("gclk"), sclk("sclk"), sin("sin"), lat("lat") {
+        SC_CTHREAD(handle_sin, gclk);
+        SC_CTHREAD(handle_lat, gclk);
     }
 
-    void handle_sin() {}
+    void handle_sin() {
+        while (true) {
+            wait();
+            gs_reg = ((gs_reg.read() << 1) & ~1) | sin;
+        }
+    }
 
-    sc_signal<bool> clk;
+    void handle_lat() {
+        while (true) {
+            wait();
+            if (lat) {
+                lat_counter = lat_counter.read() + 1;
+            } else if (lat_counter == 1) {
+                // WRTGS
+            } else if (lat_counter == 3) {
+                // LATGS
+            }
+        }
+    }
+
+    sc_in<bool> gclk;
+    sc_in<bool> sclk;
+    sc_in<bool> nrst;
+
+    sc_in<bool> sin;
+    sc_in<bool> lat;
+
+    private:
+    sc_signal<sc_bv<48>> gs_reg;
+    sc_signal<int> lat_counter;
 };
