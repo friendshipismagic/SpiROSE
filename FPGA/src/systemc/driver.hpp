e04e1b9bde872d5e057bd311f47a7a6b3c9dd3fa
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2017-11-19 17:31:04 +0100

    FPGA/systemc/driver.hpp: add constructor and sin_process

--- FPGA/src/systemc/driver.hpp
+++ FPGA/src/systemc/driver.hpp
@@ -3,6 +3,14 @@
 #include <systemc.h>
 
 class Driver : public sc_module {
+    SC_HAS_PROCESS(Driver);
+
     public:
-    inline Driver(const sc_module_name& name) : sc_module(name) {}
+    inline Driver(const sc_module_name& name) : sc_module(name) {
+        SC_CTHREAD(handle_sin, clk);
+    }
+
+    void handle_sin() {}
+
+    sc_signal<bool> clk;
 };
