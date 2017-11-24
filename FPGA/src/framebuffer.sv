435aa576b2563d43cd434868571238f9e86ede61
adrien <adrien.marcenat@telecom-paristech.fr>
2017-11-24 14:06:39 +0100

    FPGA/src/framebuffer.sv: generate counters
    Generate three counters:
    * mul_idx to know which column should be sent
    * led_idx to know which voxel we are reading
    * bit_idx to know which bit of this voxel should be sent

--- FPGA/src/framebuffer.sv
+++ FPGA/src/framebuffer.sv
@@ -20,8 +20,12 @@ module  framebuffer #(
     input      enc_sync
 );
 
-localparam IMAGE_SIZE = 80*48;
-localparam DRIVER_IMAGE_SIZE = 128;
+localparam MULTIPLEXING = 8;
+localparam LED_PER_DRIVER = 16;
+localparam DRIVER_IMAGE_SIZE = LED_PER_DRIVER*MULTIPLEXING;
+localparam ROW_SIZE = 80;
+localparam COLUMN_SIZE = 48;
+localparam IMAGE_SIZE = ROW_SIZE*COLUMN_SIZE;
 
 /*
  * The two framebuffers, while one is reading the ram the other is sending
@@ -36,8 +40,10 @@ wire  [RAM_DATA_WIDTH-1:0] writing_buffer [IMAGE_SIZE-1:0];
 logic current_buffer;
 // The write index of the buffer reading the ram
 logic [$clog2(IMAGE_SIZE)-1:0] write_idx;
-// The read index of the buffer sending the data
-logic [$clog2(IMAGE_SIZE)-1:0] read_idx;
+// The column we are currently sending
+logic [$clog2(MULTIPLEXING)-1:0] mul_idx;
+// The led we are currently sending data to
+logic [$clog2(LED_PER_DRIVER)-1:0] led_idx;
 // The current bit to send to the driver main controller
 logic [$clog2(POKER_MODE)-1:0] bit_idx;
 // Indicates that we have written a whole slice in the buffer
@@ -54,11 +60,11 @@ assign image_start_addr = enc_position*IMAGE_SIZE + RAM_BASE;
 assign new_image = enc_sync;
 
 // Read ram to fill the reading buffer
-always_ff@(posedge clk_33)
+always_ff @(posedge clk_33)
     if(~nrst) begin
         for(int i = 0; i < IMAGE_SIZE; ++i) begin
-            buffer1[i] <= '0;
-            buffer2[i] <= '0;
+            buffer1[i] = '0;
+            buffer2[i] = '0;
         end
         write_idx <= '0;
     end else begin
@@ -79,15 +85,38 @@ always_ff @(posedge clk_33)
         data <= '0;
     end else begin
         for(int i = 0; i < 30; ++i) begin
-            data[i] <= writing_buffer[read_idx][bit_idx];
+            data[i] <= writing_buffer[ROW_SIZE*led_idx + mul_idx][bit_idx];
         end
     end
 
+/*
+ * Generate counters to read the right data.
+ *
+ * - mul_idx is the current column
+ * - led_idx is the current led
+ * - bit_idx is the current bit
+ *
+ * In poker mode we send the MSB of each led first, thus bit_idx is decreased
+ * every 16 cycles. When it reaches 0 we change the current column.
+ */
 always_ff @(posedge clk_33)
     if(nrst) begin
-        read_idx <= '0;
+        mul_idx <= '0;
+        bit_idx <= POKER_MODE-1;
+        led_idx <= '0;
     end else begin
-
+        led_idx <= led_idx + 1'b1;
+        if(led_idx == LED_PER_DRIVER-1) begin
+            led_idx <= '0;
+            bit_idx <= bit_idx - 1'b1;
+            if(bit_idx == 0) begin
+                bit_idx <= POKER_MODE-1;
+                mul_idx <= mul_idx + 1'b1;
+                if(mul_idx == MULTIPLEXING-1) begin
+                    mul_idx <= '0;
+                end
+            end
+        end
     end
 
 endmodule
