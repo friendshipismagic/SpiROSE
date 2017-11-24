b3cec1f51c29c03f23efa6df5f835c847dcbb7cd
adrien <adrien.marcenat@telecom-paristech.fr>
2017-11-24 16:14:26 +0100

    FPGA/src/framebuffer.sv: pad with 0 after 16 bits
    Add an rgb_counter to know which color we are sending, and wires to
    compute the correct bit address in the voxel. Since we use 6 bits for
    the green it is not a simple modulo. Instead we use a COLOR_BASE
    localparam with the strating bits of the three colors (0,5,11) and add
    to it a color_idx that goes from 5 to 0. When the color is not green we
    substract 1 to this color_idx so it goes from 4 to 0.

--- FPGA/src/framebuffer.sv
+++ FPGA/src/framebuffer.sv
@@ -25,7 +25,8 @@ localparam LED_PER_DRIVER = 16;
 localparam ROW_SIZE = 80;
 localparam COLUMN_SIZE = 48;
 localparam IMAGE_SIZE = ROW_SIZE*COLUMN_SIZE;
-
+// We use 16-bits rgb : 5 bits red, 6 bits green, 5 bit blue.
+localparam [2:0] [15:0] COLOR_BASE = '{0,5,11};
 /*
  * The drivers are layout as follow:
  * |++++++++++++++++++++||++++++++++++++++++++|     |++++++++++++++++++++|
@@ -73,6 +74,11 @@ logic [$clog2(MULTIPLEXING)-1:0] mul_idx;
 logic [$clog2(LED_PER_DRIVER)-1:0] led_idx;
 // The current bit to send to the driver main controller
 logic [$clog2(POKER_MODE)-1:0] bit_idx;
+wire  [$clog2(POKER_MODE)-1:0] color_bit_idx;
+// The color we are currently sending
+logic [1:0] rgb_idx;
+logic [$clog2(RAM_DATA_WIDTH)-1:0] color_addr;
+logic [$clog2(IMAGE_SIZE)-1:0] voxel_addr;
 // Indicates that we have written a whole slice in the buffer
 wire has_reached_end;
 // Indicates that the buffer needs to be swapped
@@ -80,13 +86,13 @@ wire new_image;
 // Start address of the next image to display
 wire[RAM_ADDR_WIDTH-1:0] image_start_addr;
 
+
+// Read ram to fill the reading buffer
 assign reading_buffer = current_buffer ? buffer1 : buffer2;
-assign writing_buffer = current_buffer ? buffer2 : buffer1;
 assign has_reached_end = write_idx == IMAGE_SIZE-1;
 assign image_start_addr = enc_position*IMAGE_SIZE + RAM_BASE;
 assign new_image = enc_sync;
 
-// Read ram to fill the reading buffer
 always_ff @(posedge clk_33)
     if(~nrst) begin
         for(int i = 0; i < IMAGE_SIZE; ++i) begin
@@ -113,18 +119,36 @@ always_ff @(posedge clk_33)
  * one is at address n+2 becasue of the interleaving described above, hence we
  * add 2*mul_idx to the base address to get the right column. Then we add
  * led_idx*ROW_SIZE to get the right line.
+ *
+ * The color_bit_idx goes from 5 to 0, thus if we add the base address for the
+ * green color for instance we get bits 10 to 5. The red and blue color have
+ * 5 bits instead of 6, hence we need to substract 1 to color_addr.
  */
+assign color_bit_idx = (bit_idx >= 3) ? bit_idx - 3 : 0;
+assign voxel_addr = 2*mul_idx + led_idx*ROW_SIZE;
+assign color_addr = color_bit_idx + COLOR_BASE[rgb_idx] - (rgb_idx != 1);
+assign writing_buffer = current_buffer ? buffer2 : buffer1;
+
 always_ff @(posedge clk_33)
     if(~nrst) begin
         data <= '0;
     end else begin
-        for(int i = 0; i < 30; ++i) begin
-            data[i] <= writing_buffer[DRIVER_BASE[i] + 2*mul_idx + led_idx*ROW_SIZE][bit_idx];
+        // Since we only have 16 bit per led we have to pad with 0.
+        data <= '0;
+        /*
+         * If we haven't sent 16 bit yet we don't pad with 0.
+         * rgb_idx == 1 means we are sending the green color which has 6 bits
+         * instead of 5.
+         */
+        if(bit_idx > 3 || (rgb_idx == 1 && bit_idx == 3)) begin
+            for(int i = 0; i < 30; ++i) begin
+                data[i] <= writing_buffer[DRIVER_BASE[i] + voxel_addr][color_addr];
+            end
         end
     end
 
 /*
- * Generate counters to read the right data.
+ * Generate counters to send the right data.
  *
  * - mul_idx is the current column
  * - led_idx is the current led
@@ -134,20 +158,24 @@ always_ff @(posedge clk_33)
  * every 16 cycles. When it reaches 0 we change the current column.
  */
 always_ff @(posedge clk_33)
-    if(nrst) begin
+    if(~nrst) begin
+        rgb_idx <= '0;
         mul_idx <= '0;
         bit_idx <= POKER_MODE-1;
         led_idx <= '0;
     end else begin
-        led_idx <= led_idx + 1'b1;
-        if(led_idx == LED_PER_DRIVER-1) begin
-            led_idx <= '0;
-            bit_idx <= bit_idx - 1'b1;
-            if(bit_idx == 0) begin
-                bit_idx <= POKER_MODE-1;
-                mul_idx <= mul_idx + 1'b1;
-                if(mul_idx == MULTIPLEXING-1) begin
-                    mul_idx <= '0;
+        rgb_idx <= rgb_idx + 1'b1;
+        if(rgb_idx == 2) begin
+            led_idx <= led_idx + 1'b1;
+            if(led_idx == LED_PER_DRIVER-1) begin
+                led_idx <= '0;
+                bit_idx <= bit_idx - 1'b1;
+                if(bit_idx == 0) begin
+                    bit_idx <= POKER_MODE-1;
+                    mul_idx <= mul_idx + 1'b1;
+                    if(mul_idx == MULTIPLEXING-1) begin
+                        mul_idx <= '0;
+                    end
                 end
             end
         end
