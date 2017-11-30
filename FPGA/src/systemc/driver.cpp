ffef9a38bae53b91e27c76b8d25f6690f07df51c
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2017-11-30 15:31:45 +0100

    FPGA/systemc/driver: use naming convention from CONTRIBUTING.md

--- FPGA/src/systemc/driver.cpp
+++ FPGA/src/systemc/driver.cpp
@@ -2,148 +2,147 @@
 
 #include <iostream>
 
-sc_bv<2> Driver::get_lodth() const { return fc_data.read()(1, 0); }
+sc_bv<2> Driver::getLodth() const { return fcData.read()(1, 0); }
 
-sc_bv<2> Driver::get_td0() const { return fc_data.read()(3, 2); }
+sc_bv<2> Driver::getTd0() const { return fcData.read()(3, 2); }
 
-bool Driver::get_group() const { return fc_data.read().bit(4).to_bool(); }
+bool Driver::getGroup() const { return fcData.read().bit(4).to_bool(); }
 
-bool Driver::get_xrefresh() const { return fc_data.read().bit(5).to_bool(); }
-
-bool Driver::get_sel_gck_edge() const {
-    return fc_data.read().bit(6).to_bool();
+bool Driver::getXrefreshDisabled() const {
+    return fcData.read().bit(5).to_bool();
 }
 
-bool Driver::get_sel_pchg() const { return fc_data.read().bit(7).to_bool(); }
+bool Driver::getSelGckEdge() const { return fcData.read().bit(6).to_bool(); }
 
-bool Driver::get_espwm() const { return fc_data.read().bit(8).to_bool(); }
+bool Driver::getSelPchg() const { return fcData.read().bit(7).to_bool(); }
 
-bool Driver::get_lgse3() const { return fc_data.read().bit(9).to_bool(); }
+bool Driver::getEspwm() const { return fcData.read().bit(8).to_bool(); }
 
-bool Driver::get_sel_sck_edge() const {
-    return fc_data.read().bit(10).to_bool();
-}
+bool Driver::getLgse3() const { return fcData.read().bit(9).to_bool(); }
 
-sc_bv<3> Driver::get_lgse1() const { return fc_data.read()(13, 11); }
+bool Driver::getSelSckEdge() const { return fcData.read().bit(10).to_bool(); }
 
-sc_bv<9> Driver::get_ccb() const { return fc_data.read()(22, 14); }
+sc_bv<3> Driver::getLgse1() const { return fcData.read()(13, 11); }
 
-sc_bv<9> Driver::get_ccg() const { return fc_data.read()(31, 23); }
+sc_bv<9> Driver::getCcb() const { return fcData.read()(22, 14); }
 
-sc_bv<9> Driver::get_ccr() const { return fc_data.read()(40, 32); }
+sc_bv<9> Driver::getCcg() const { return fcData.read()(31, 23); }
 
-sc_bv<3> Driver::get_bc() const { return fc_data.read()(43, 41); }
+sc_bv<9> Driver::getCcr() const { return fcData.read()(40, 32); }
 
-bool Driver::get_poker_mode() const { return fc_data.read().bit(44).to_bool(); }
+sc_bv<3> Driver::getBc() const { return fcData.read()(43, 41); }
 
-sc_bv<3> Driver::get_lgse2() const { return fc_data.read()(47, 45); }
+bool Driver::getPokerMode() const { return fcData.read().bit(44).to_bool(); }
 
-void Driver::check_assert() {
+sc_bv<3> Driver::getLgse2() const { return fcData.read()(47, 45); }
+
+void Driver::checkAssert() {
     while (true) {
         // Can't use XREFRESH and ESPWM in poker mode, p17, 3.4.3
-        if (get_poker_mode() && (get_xrefresh() || get_espwm())) {
-            std::cout << "FATAL: XREFRESH(" << get_xrefresh() << ") "
-                      << "and ESPWM(" << get_espwm() << ") "
-                      << "have to be 0 in poker mode" << std::endl;
+        if (getPokerMode() && (getXrefreshDisabled() || getEspwm())) {
+            std::cout << "FATAL: XREFRESH(" << getXrefreshDisabled() << ") "
+                      << "and ESPWM(" << getEspwm() << ") "
+                      << "have to be 0 (deactivated) in poker mode"
+                      << std::endl;
             exit(-1);
         }
         wait();
     }
 }
 
-void Driver::handle_sin() {
+void Driver::handleSin() {
     while (true) {
         wait();
-        shift_reg = ((shift_reg.read() << 1) & ~1) | sin;
+        shiftReg = ((shiftReg.read() << 1) & ~1) | sin;
     }
 }
 
-void Driver::handle_lat() {
-    gs_addr_counter = GS_ADDR_COUNTER_ORIGIN;
+void Driver::handleLat() {
+    gsAddrCounter = GS_ADDR_COUNTER_ORIGIN;
 
     while (true) {
         wait();
         if (lat) {
-            lat_counter = lat_counter.read() + 1;
-        } else if (lat_counter == 1) {
+            latCounter = latCounter.read() + 1;
+        } else if (latCounter == 1) {
             // WRTGS, write to GS data at the GS counter position
-            write_to_bank(GS1, gs_addr_counter.read(), shift_reg);
-            gs_addr_counter = gs_addr_counter.read() - 1;
-            lat_counter = 0;
-        } else if (lat_counter == 3) {
+            writeToBank(GS1, gsAddrCounter.read(), shiftReg);
+            gsAddrCounter = gsAddrCounter.read() - 1;
+            latCounter = 0;
+        } else if (latCounter == 3) {
             // LATGS, write to GS data
-            write_to_bank(GS1, gs_addr_counter.read(), shift_reg);
-            if (get_xrefresh() == 0) {
+            writeToBank(GS1, gsAddrCounter.read(), shiftReg);
+            if (getXrefreshDisabled() == 0) {
                 // latch GS1 to GS2 when gs_addr_counter = 65536
-                if (gs_addr_counter == 0) {
-                    gs2_data = gs1_data;
+                if (gsAddrCounter == 0) {
+                    gs2Data = gs1Data;
                 }
             } else {
                 // latch GS1 to GS2
                 // reset gs_addr_counter (right after the else)
                 // TODO: put outx = 0 if we need to test it
-                gs2_data = gs1_data;
+                gs2Data = gs1Data;
             }
-            gs_addr_counter = GS_ADDR_COUNTER_ORIGIN;
-            line_counter = line_counter.read() + 1;
-            lat_counter = 0;
-        } else if (lat_counter == 5) {
+            gsAddrCounter = GS_ADDR_COUNTER_ORIGIN;
+            lineCounter = lineCounter.read() + 1;
+            latCounter = 0;
+        } else if (latCounter == 5) {
             // WRTFC
-            fc_data = shift_reg;
-            lat_counter = 0;
-        } else if (lat_counter == 7) {
+            fcData = shiftReg;
+            latCounter = 0;
+        } else if (latCounter == 7) {
             // LINERESET
-            write_to_bank(GS1, gs_addr_counter.read(), shift_reg);
-            line_counter = 0;
-            gs_addr_counter = GS_ADDR_COUNTER_ORIGIN;
-            if (get_xrefresh() == 0) {
+            writeToBank(GS1, gsAddrCounter.read(), shiftReg);
+            lineCounter = 0;
+            gsAddrCounter = GS_ADDR_COUNTER_ORIGIN;
+            if (getXrefreshDisabled() == 0) {
                 // TODO: copy everything when GS counter is 65536;
             } else {
-                gs2_data.write(gs1_data);
-                gs_data_counter = 0;
+                gs2Data.write(gs1Data);
+                gsDataCounter = 0;
                 // TODO: OUTx forced off
             }
-            lat_counter = 0;
-        } else if (lat_counter == 11) {
+            latCounter = 0;
+        } else if (latCounter == 11) {
             // READFC
             // It's the following line, but we can't do it because it is driven
-            // TODO: handle_sout using either fc_data or shift_reg (later)
-            // by handle_sin shift_reg.write(fc_data);
-            lat_counter = 0;
-        } else if (lat_counter == 13) {
+            // TODO: handle_sout using either fcData or shift_reg (later)
+            // by handle_sin shift_reg.write(fcData);
+            latCounter = 0;
+        } else if (latCounter == 13) {
             // TMGRST
-            gs_data_counter = 0;
-            line_counter = 0;
+            gsDataCounter = 0;
+            lineCounter = 0;
             // TODO: force output off if we need to test it
-            lat_counter = 0;
-        } else if (lat_counter == 15) {
+            latCounter = 0;
+        } else if (latCounter == 15) {
             // FCWRTEN, enable to write to FC data latch
-            current_mode = MODE_FCWRT;
-            lat_counter = 0;
+            currentMode = MODE_FCWRT;
+            latCounter = 0;
         } else {
-            assert(lat_counter == 0 && "Invalid lat counter value");
+            assert(latCounter == 0 && "Invalid lat counter value");
         }
     }
 }
 
-void Driver::handle_gclk() {
-    if (get_xrefresh() == 0) {
+void Driver::handleGclk() {
+    if (getXrefreshDisabled() == 0) {
         // auto-refresh is enabled, so we automatically latch in the end
         std::cout << "xrefresh is activated and shouldn't be" << std::endl;
     } else {
         // auto-refresh is disabled, we shut everything off as soon as we reach
         // the end of the counter
-        gs_data_counter = gs_data_counter.read() + 1;
+        gsDataCounter = gsDataCounter.read() + 1;
     }
 }
 
-void Driver::write_to_bank(driver_bank_t bank, int buffer_id,
-                           const RegBuff& buffer) {
-    auto& gs = (bank == GS1) ? gs1_data : gs2_data;
+void Driver::writeToBank(driver_bank_t bank, int bufferId,
+                         const RegBuff& buffer) {
+    auto& gs = (bank == GS1) ? gs1Data : gs2Data;
     auto vec = gs.read();
-    if (!get_poker_mode()) {
+    if (!getPokerMode()) {
         // Calculate the slice where to write in GS
-        int end = REG_SIZE * (1 + buffer_id) - 1;
+        int end = REG_SIZE * (1 + bufferId) - 1;
 
         // We need to extract the data from signal to apply range
         // operator
@@ -160,7 +159,7 @@ void Driver::write_to_bank(driver_bank_t bank, int buffer_id,
                 // the MSB first
                 // + the 48 bits buffer is splitted into three 16 bits parts,
                 // one for each color, so j*16 translates to the right buffer
-                auto idx = i * REG_SIZE - (GS_NB_BUFFER - buffer_id) - j * 16;
+                auto idx = i * REG_SIZE - (GS_NB_BUFFER - bufferId) - j * 16;
                 vec[idx] = buffer[i];
             }
         }
@@ -168,6 +167,6 @@ void Driver::write_to_bank(driver_bank_t bank, int buffer_id,
     gs.write(vec);
 }
 
-Driver::GSBuff Driver::get_gs1_data() const { return gs1_data.read(); }
-Driver::GSBuff Driver::get_gs2_data() const { return gs2_data.read(); }
-Driver::RegBuff Driver::get_fc_data() const { return fc_data.read(); }
+Driver::GSBuff Driver::getGs1Data() const { return gs1Data.read(); }
+Driver::GSBuff Driver::getGs2Data() const { return gs2Data.read(); }
+Driver::RegBuff Driver::getFcData() const { return fcData.read(); }
