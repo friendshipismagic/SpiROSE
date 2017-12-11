ace48eefac424c196f83cb33a5f5cc6e8d908ed8
adrien <adrien.marcenat@telecom-paristech.fr>
2017-12-11 23:55:01 +0100

    renderer/tests/: use xvfb to create an X server

--- tools/renderer/tests/tests.sh
+++ tools/renderer/tests/tests.sh
@@ -1,8 +1,9 @@
 # Tests for the SpiROSE renderer
 
 # Start the renderer just to know if the shaders are loaded
-cd ../
-./rose-renderer-poc -x &
-pid=$!
-kill -9 $pid
+Xvfb :1 -screen 0 1280x720x24 +extension GLX &
+DISPLAY=:1 XDG_RUNTIME_DIR=/tmp ./rose-renderer-poc -x &
+sleep 5
+kill %2
+kill %1
 
