9def78518c555b60913892b419bee8efddc2eb18
adrien <adrien.marcenat@telecom-paristech.fr>
2017-12-12 01:02:02 +0100

    renderer/tests/: test if something is displayed
    Use imagemahick to take a screenshot and get the color of the center
    pixel. exit with error if it is black.

--- tools/renderer/tests/tests.sh
+++ tools/renderer/tests/tests.sh
@@ -1,9 +1,20 @@
 # Tests for the SpiROSE renderer
 
+function finish {
+    kill %2
+    kill %1
+    rm -rf grab.jpg info.txt
+}
+trap finish EXIT
+
 # Start the renderer just to know if the shaders are loaded
 Xvfb :1 -screen 0 1280x720x24 +extension GLX &
 DISPLAY=:1 XDG_RUNTIME_DIR=/tmp ./rose-renderer-poc -x &
 sleep 5
-kill %2
-kill %1
 
+# Take a screenshot with imagemagick
+DISPLAY=:1 import -window root grab.jpg
+# Get the color of the center pixel
+convert grab.jpg[1x1+640+360] -format "%[fx:int(255*r)],%[fx:int(255*g)],%[fx:int(255*b)]" info.txt
+# Check that this pixel is not black
+grep black info.txt && exit 1 || /bin/true
