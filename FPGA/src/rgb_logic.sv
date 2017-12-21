ca95414c0f9635ec35c3b9c9b4e5306b44ee2eb3
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2017-12-21 22:42:10 +0100

    FPGA/src/rgb_logic.sv: split processes
    Split rgb_logic into different processes. It doesn't handle writing
    correctly, as it resets the address at the end of the frame whilst the
    ram can't store a full frame. It should have a parameter telling the
    maximum number of µBlocks before cycling.

--- FPGA/src/rgb_logic.sv
+++ FPGA/src/rgb_logic.sv
@@ -14,30 +14,40 @@ module rgb_logic #(
     // TODO Check RAM Address is on 16b
     output [RAM_ADDR_WIDTH-1:0] ramAddr,
     output [RAM_DATA_WIDTH-1:0] ramData,
-    output writeEnable
+    output logic writeEnable
 );
 
 localparam IMAGE_SIZE = 80*48;
 
 logic isBlanking;
-assign isBlanking = hsync || vsync;
+assign isBlanking = hsync | vsync;
+
+logic isNewFrame;
+assign isNewFrame = hsync & vsync;
 
 always_ff @(posedge rgbClk)
-    if(~nrst) begin
-        ramAddr <= '0;
+    if(~nrst)
         ramData <= '0;
-        writeEnable <= '0;
-    end else begin
-        // We don't write anything in blanking area
-        writeEnable <= '0;
-        if(~isBlanking) begin
-            writeEnable <= 1'b1;
-            // We use 5 bits for the red, 6 for the green, and 5 for the blue
-            ramData <= {rgb[23:19], rgb[15:10], rgb[7:3]};
-            // TODO: handle write address
+    else
+        // We don't write anything in blanking area but it is controlled by writeEnable signal
+        // We use 5 bits for the red, 6 for the green, and 5 for the blue
+        ramData <= {rgb[23:19], rgb[15:10], rgb[7:3]};
+
+always_ff @(posedge rgbClk or negedge nrst)
+    if(~nrst)
+        ramAddr <= '0;
+    else begin
+        if(isNewFrame)
+            ramAddr <= '0;
+        if(~isBlanking)
             ramAddr <= ramAddr + 1'b1;
-        end
     end
+
+always_ff @(posedge rgbClk or negedge nrst)
+    if(~nrst)
+        writeEnable <= '0;
+    else
+        writeEnable <= ~isBlanking;
 
 endmodule
 
