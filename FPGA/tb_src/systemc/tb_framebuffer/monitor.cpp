8a74575ca6aab2bc0b8a41517de579cdd935a2e3
Vincent Charbonnieras <vincent.charbonnieras@telecom-paristech.fr>
2017-12-22 19:11:31 +0100

    FPGA/tb_src/tb_framebuffer: driver LUT, output data handling

--- FPGA/tb_src/systemc/tb_framebuffer/monitor.cpp
+++ FPGA/tb_src/systemc/tb_framebuffer/monitor.cpp
@@ -3,31 +3,108 @@
 
 #include "monitor.hpp"
 
+static uint8_t fbOutput[30][9][48];
+
 void Monitor::reset() {
     nrst = 0;
     wait(clk33.posedge());
     nrst = 1;
 }
 
-void Monitor::runTests() {
-
+void Monitor::continuousCycleCount() {
+    while(1) {
+        if (!nrst) {
+            cycleCounter = 0; 
+        }
+        else {
+            if (cycleCounter == 512) {
+                cycleCounter = 0;
+            }
+            cycleCounter++;
+        }
+        wait();
+    }
 }
 
+void Monitor::storeOutputData() {
+    while(1) {
+        if (!nrst) {
+            // Reset the value of the output test array
+            int i, j, k;
+            for (i = 0; i < 30; i++) {
+                for (j = 0; j < 9; j++) {
+                    for (k = 0; k < 48; k++) {
+                        fbOutput[i][j][k] = 0;
+                    }
+                }
+            }
+        }
+        else if (cycleCounter >= 0 
+                    && cycleCounter < 440
+                    && cycleCounter != 48
+                    && cycleCounter != 97
+                    && cycleCounter != 146
+                    && cycleCounter != 195
+                    && cycleCounter != 244
+                    && cycleCounter != 293
+                    && cycleCounter != 342
+                    && cycleCounter != 391) {
+            for (int i = 0; i < 30; i++) {
+                fbOutput[i][cycleCounter/49][cycleCounter%49] = (data >> i) & 0x1;
+            }
+        }
+        wait();
+    }
+}
+
+void Monitor::checkWRTGSBlanking() {
+    while (1) {
+        if (cycleCounter == 48 && cycleCounter == 48+49 /* ... */) {
+
+        }
+        wait();
+    }
+}
+
+
+/* 
+ * The aims of this testing method is to check that the data
+ * that is output by the framebuffer module is correct,
+ * relatively to the frames structure and the order in which
+ * the data is meant to be ouput aka in poker mode.
+ * The idea of the framebuffer is that it reads 30 16-bit wide
+ * data from RAM and at the next cycle outputs it.
+ * 
+ * Besides, since the pin assignment for the drivers was changed,
+ * look-up tables need to be implemented in the framebuffer module
+ * so as to modify the order of the data half words between the moment 
+ * they are read in RAM and they are sent to the driver controller.
+ * Taking the led panel schematic notations, there are two different
+ * driver pin assignments for {UB0, {UB1, UB2}}. The 16-bit data is 
+ * thus changed depending of which driver it corresponds to.
+ * 
+ * The signals that are used are data and sync.
+ */
 void Monitor::checkDataIntegrity() {
 
 }
 
-void Monitor::checkRamAccess() {
 
-}
 
+/* 
+ * To emulate the RAM, we simply have an emulator that gives
+ * back data as a function of the address to the framebuffer.
+ * It will be tested, to be sure that the framebuffer correctly
+ * sends data forward to the driver controller in poker mode
+ * and in the right order.
+ *
+ * The real RAM structure is as follows:
+ * The µblocks are stored one after another (µblock0, ... , µblock18)
+ * Each µblock has 80*46 pixels, each pixel being 16-bit wide, aka RGB565.
+ * Poker mode implies that the pixels are cut and sent to each driver
+ * in the following order:
+ * B15[n] || G15[n] || R15[n] || ... || B0[n] || G0[n] || R0[n]
+ */
 void Monitor::ramEmulator() {
-    /* To emulate the RAM, we simply have an emulator, that gives
-     * back data as a function of the address to the framebuffer.
-     * It will be tested, to be sure that the framebuffer correctly
-     * sends data forward to the driver controller in poker mode
-     * and in the right order.
-     */
     ram_data = ram_addr % (1 << 16);
-    cout << ram_addr << endl;
 }
--- FPGA/tb_src/systemc/tb_framebuffer/monitor.hpp
+++ FPGA/tb_src/systemc/tb_framebuffer/monitor.hpp
@@ -1,28 +1,71 @@
+#pragma once
+
 #include "systemc.h"
 
 SC_MODULE(Monitor) {
     SC_CTOR(Monitor) {
-        SC_CTHREAD(runTests, clk33.pos());
         SC_METHOD(ramEmulator);
         sensitive << ram_addr;
+        SC_CTHREAD(continuousCycleCount, clk33.pos());
+        SC_CTHREAD(storeOutputData, clk33.pos());
+        SC_CTHREAD(checkWRTGSBlanking, clk33.pos());
     }
 
     sc_in<bool> clk33;
     sc_out<bool> nrst;
 
+    // Signal indicating when a whole slice has been sent
     sc_in<bool> sync;
+    // Serial data sent over the 30 drivers
     sc_in<unsigned int> data;
 
     sc_in<unsigned int> ram_addr;
     sc_out<unsigned int> ram_data;
 
+    // Position of the rotary panel (256 positions)
     sc_out<unsigned int> enc_position;
     sc_out<bool> enc_sync;
 
-    void runTests();
+    /*
+     * Description of the operations needed for a single slice:
+     * There a 8 identical 512-cycle-long processes thar take place
+     * for a slide. The number is 8 since we have a 8-multiplexing.
+     * During the i-th 512-cycle-long process, the framebuffer outputs
+     * the i-th multiplexed LED data in poker mode during the first
+     * 440 cycles, with a 1-cycle blanking during each 48-bit wide 
+     * output data, corresponding to the required latency for the
+     * driver WRTGS command. The remaining 72 cycles are needed to
+     * have the 512 GCLK cycles.
+     */
+
+
+    /* 
+     * cycleCounter is a variable that monitors the location inside
+     * each 512-bit wide process.
+     * continuousCycleCount is a CTHREAD that keeps incrementing cycleCount
+     * until it reaches 512. This counter isthe base of synchronisation for
+     * all other implemented threads/methods, since the framebuffer is 
+     * supposed to be synchronised on 512 cycles.
+     */
+    int cycleCounter;
+    void continuousCycleCount();
+
+    /* 
+     * Function that stores the output data of the framebuffer during
+     * the period it is emitted, aka between cycleCounter=0 and 440.
+     */
+    void storeOutputData();
+
     void checkDataIntegrity();
-    void checkRamAccess();
     void ramEmulator();
+
+    /* 
+     * Check that every time a 48-bit wide section is transmitted, a 
+     * blanking cycle is inserted to fit the timing constraints 
+     * of the driver. This test can be achieved by verifying that the
+     * data output remains stable over the 2 concerned clock cycles.
+     */
+    void checkWRTGSBlanking();
 
     private:
 
