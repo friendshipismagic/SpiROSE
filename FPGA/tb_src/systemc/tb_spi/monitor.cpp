2c9531d0c76e575d189d304fe345aab939fa350d
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2018-01-08 12:03:01 +0100

    FPGA/tb_src/systemc/tb_spi/monitor: use sc_bv instead of int for checker

--- FPGA/tb_src/systemc/tb_spi/monitor.cpp
+++ FPGA/tb_src/systemc/tb_spi/monitor.cpp
@@ -7,14 +7,15 @@
 constexpr int SPI_CYCLES = 8;
 
 namespace {
-void checkValueEq(int left, int right) {
+template <int Size>
+void checkValueEq(sc_bv<Size> left, sc_bv<Size> right) {
     if (left == right) {
         SC_REPORT_INFO("spi", "left and right are identical");
         return;
     }
     std::ostringstream s;
-    s << "the value is different from expected, received " << sc_bv<8>(left)
-      << ", expected " << sc_bv<8>(right);
+    s << "the value is different from expected, received " << left
+      << ", expected " << right;
     SC_REPORT_ERROR("spi", s.str().c_str());
 }
 }  // namespace
@@ -30,35 +31,43 @@ void Monitor::runTests() {
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0b00000000);
 
-    checkValueEq(capturedValue, 0b11111111);
+    checkValueEq<8>(capturedValue, "11111111");
 
     ss = true;
 
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0b11111111);
 
-    checkValueEq(capturedValue, 0b11111111);
+    checkValueEq<8>(capturedValue, "11111111");
 
     ss = false;
 
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0b11111111);
 
-    checkValueEq(capturedValue, 0b11111111);
+    checkValueEq<8>(capturedValue, "11111111");
 
     ss = false;
 
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0b11111111);
 
-    checkValueEq(capturedValue, 0b11111111);
+    checkValueEq<8>(capturedValue, "11111111");
 
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0xBF);
 
+    for (int i = 0; i < 7; ++i) {
+        sendCommand(0xA0);
+    }
+
+    checkValueEq<48>(configOut.read(), 0xA0A0A0A0A0A0);
+
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
     sendCommand(0x4C);
 
+    sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
+    sendCommand(0x4C);
     while (true) wait();
 }
 
@@ -71,11 +80,12 @@ void Monitor::sendReset() {
 }
 
 void Monitor::sendCommand(char value) {
+    // Sample data at the rising edge, so we change data at the falling edge
     sc_assert(clk == 1);
     for (int i = 0; i < 2 * SPI_CYCLES; ++i) {
         sck = clk;
+        mosi = (value >> (SPI_CYCLES - i / 2 - 1)) & 1;
         wait(clk.value_changed_event());
-        mosi = value >> (SPI_CYCLES - i / 2 - 1) & 1;
     }
     sck = 0;
 }
