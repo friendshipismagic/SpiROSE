fbc2c794a7dd304a4f41b9f7f2b25552583f6b9c
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2018-01-11 21:37:37 +0100

    FPGA/tb_src/systemc/tb_rgb_logic: test rgb logic data integrity

--- FPGA/tb_src/systemc/tb_rgb_logic/monitor.cpp
+++ FPGA/tb_src/systemc/tb_rgb_logic/monitor.cpp
@@ -4,18 +4,108 @@
 #include "monitor.hpp"
 
 void Monitor::runTests() {
+    rgbEnable = false;
     sendReset();
 
     // wait some cycles before sending anything
-    wait(200);
+    for (int i = 0; i < 200; ++i) {
+        wait(clk.posedge_event());
+    }
 
-    while (true) wait();
+    // Start sending 8 frames without rgbEnable
+    sendFrame();
+    checkRAM(false);
+    cleanRAM();
+
+    rgbEnable = true;
+    // Start sending frames with rgbEnable
+    sendFrame();
+    checkRAM(true);
+    cleanRAM();
 }
 
 void Monitor::sendReset() {
     nrst = 0;
     for (int i = 0; i < 10; ++i) {
-        wait(clk.posedge());
+        wait();
     }
     nrst = 1;
+}
+
+void Monitor::captureRAM() {
+    while (true) {
+        if (writeEnable) {
+            ram.resize(std::max(ram.size(), (size_t)ramAddr.read()));
+            ram[ramAddr] = ramData;
+        }
+        wait();
+    }
+}
+
+void Monitor::checkRgbEnable() {
+    while (true) {
+        if (writeEnable && !rgbEnable) {
+            std::string msg;
+            msg += "rgb_logic module tried to write the ram ";
+            msg += "although it was disabled (rgb_enable = 0)";
+            SC_REPORT_ERROR("rgb", msg.c_str());
+            return;
+        }
+        wait();
+    }
+}
+
+void Monitor::sendFrame() {
+    hsync = true;
+    vsync = true;
+    // Start sending 8 frames without rgbEnable
+    for (int k = 0; k < 8; ++k) {
+        for (int y = 0; y < FRAME_HEIGHT; ++y) {
+            for (int x = 0; x < FRAME_WIDTH; ++x) {
+                rgb = y * FRAME_WIDTH + x;
+                wait(clk.posedge_event());
+            }
+            hsync = false;
+            wait(clk.posedge_event());
+            hsync = true;
+        }
+    }
+    vsync = false;
+    for (int x = 0; x < FRAME_WIDTH; ++x) {
+        wait(clk.posedge_event());
+    }
+    hsync = false;
+    wait(clk.posedge_event());
+}
+
+void Monitor::checkRAM(bool zeroed) {
+    auto f = [this, zeroed](int k, int x, int y) {
+        if (zeroed) return 0;
+        return value(k, x, y);
+    };
+
+    for (int k = 0; k < 8; ++k) {
+        for (int y = 0; y < FRAME_HEIGHT; ++y) {
+            for (int x = 0; x < FRAME_WIDTH; ++x) {
+                std::string msg;
+                msg += "Incorrect RAM value at ";
+                msg += std::to_string(value(k, x, y));
+                msg += ", expected ";
+                msg += std::to_string(f(k, x, y));
+                msg += " and got ";
+                msg += ram[value(k, x, y)];
+                if (ram[value(k, x, y)] != f(k, x, y)) {
+                    SC_REPORT_ERROR("rgb_logic", msg.c_str());
+                }
+            }
+        }
+    }
+}
+
+void Monitor::cleanRAM() {
+    for (auto& x : ram) x = 0;
+}
+
+int Monitor::value(int k, int x, int y) {
+    return y * FRAME_WIDTH + x + k * FRAME_WIDTH * FRAME_HEIGHT;
 }
--- FPGA/tb_src/systemc/tb_rgb_logic/monitor.hpp
+++ FPGA/tb_src/systemc/tb_rgb_logic/monitor.hpp
@@ -5,10 +5,20 @@
 
 #include "driver.hpp"
 
+constexpr int FRAME_WIDTH = 40;
+constexpr int FRAME_HEIGHT = 48;
+
 SC_MODULE(Monitor) {
-    SC_CTOR(Monitor) : nrst("nrst") { SC_CTHREAD(runTests, clk.pos()); }
+    SC_CTOR(Monitor) : nrst("nrst") {
+        ram.resize(8 * FRAME_WIDTH * FRAME_HEIGHT);
+        SC_THREAD(runTests);
+        SC_CTHREAD(checkRgbEnable, clk.pos());
+        SC_CTHREAD(captureRAM, clk.pos());
+    }
 
     void runTests();
+    void captureRAM();
+    void checkRgbEnable();
 
     sc_out<bool> nrst;
     sc_in<bool> clk;
@@ -26,4 +36,10 @@ SC_MODULE(Monitor) {
 
     private:
     void sendReset();
+    void sendFrame();
+    void checkRAM(bool zeored);
+    void cleanRAM();
+    int value(int k, int x, int y);
+
+    std::vector<uint32_t> ram;
 };
