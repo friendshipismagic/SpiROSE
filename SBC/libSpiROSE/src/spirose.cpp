873a0166f27fe0a171e04c194aa132f533241e77
alexis.bauvin <alexis.bauvin@telecom-paristech.fr>
2018-01-12 15:02:05 +0100

    lib: Implement createWindow function

--- SBC/libSpiROSE/src/spirose.cpp
+++ SBC/libSpiROSE/src/spirose.cpp
@@ -1,15 +1,40 @@
+#include <cmath>
+
 #include "spirose.h"
 
 namespace spirose {
 
 GLFWwindow* createWindow(int resW, int resH, int resC) {
-    // TODO
-    return nullptr;
+    // Configure window. This depends on whether we are using GL or GLES
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+#ifdef GLES
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+#else
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, false);
+#endif
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    glm::vec2 size = windowSize(resW, resH, resC);
+    GLFWwindow* window =
+        glfwCreateWindow(size.x, size.y, "SpiROSE", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+#if !defined(GLES)
+    // Load GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+#endif
+
+    return window;
 }
 
 glm::vec2 windowSize(int resW, int resH, int resC) {
-    // TODO
-    return glm::vec2(0);
+    int h = int(sqrt(resC)) & ~1;
+    return glm::vec2(resC / h, h) * glm::vec2(resW, resH);
 }
 
 }  // namespace spirose
