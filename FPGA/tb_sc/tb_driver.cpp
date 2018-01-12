b274bb69b3cdd6e14b453fc974976021cf5aa3cf
Vincent Charbonnieras <vincent.charbonnieras@telecom-paristech.fr>
2018-01-12 09:45:39 +0100

    FPGA/tb_sc/tb_driver: test config integrity in config test loop

--- FPGA/tb_sc/tb_driver.cpp
+++ FPGA/tb_sc/tb_driver.cpp
@@ -82,28 +82,48 @@ int sc_main(int, char **) {
      */
     for (int i = 0; i < 10; i++) {
         testConfig = {
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
+            (uint32_t)rand() % 4,
+            (uint32_t)rand() % 4,
+            (uint32_t)rand() % 2,
             1,
-            (uint32_t)rand(),
-            (uint32_t)rand(),
+            (uint32_t)rand() % 2,
+            (uint32_t)rand() % 2,
             1,
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
-            (uint32_t)rand(),
+            (uint32_t)rand() % 2,
+            (uint32_t)rand() % 2,
+            (uint32_t)rand() % 8,
+            (uint32_t)rand() % 512,
+            (uint32_t)rand() % 512,
+            (uint32_t)rand() % 512,
+            (uint32_t)rand() % 8,
+            (uint32_t)rand() % 2,
+            (uint32_t)rand() % 8,
         };
         sendSequence(configWriteEnableSequence, &sin, &lat, T);
         writeConfigSequence = make_WRTCFG(testConfig);
         sendSequence(writeConfigSequence, &sin, &lat, T);
         lat.write(0);
         sc_start(T);
+
+        // Test the configuration register FC
+        printf("Testing configuration of the driver...");
+        assert(testConfig.LODVTH == driver.getLodth().to_uint());
+        assert(testConfig.SEL_TD0 == driver.getTd0().to_uint());
+        assert(testConfig.SEL_GDLY == driver.getGroup());
+        assert(testConfig.XREFRESH == driver.getXrefreshDisabled());
+        assert(testConfig.SEL_GCK_EDGE == driver.getSelGckEdge());
+        assert(testConfig.SEL_PCHG == driver.getSelPchg());
+        assert(testConfig.ESPWM == driver.getEspwm());
+        assert(testConfig.LGSE3 == driver.getLgse3());
+        assert(testConfig.SEL_SCK_EDGE == driver.getSelSckEdge());
+        assert(testConfig.LGSE1 == driver.getLgse1().to_uint());
+        assert(testConfig.CCB == driver.getCcb().to_uint());
+        assert(testConfig.CCG == driver.getCcg().to_uint());
+        assert(testConfig.CCR == driver.getCcr().to_uint());
+        assert(testConfig.BC == driver.getBc().to_uint());
+        assert(testConfig.POKER == driver.getPokerMode());
+        assert(testConfig.LGSE2 == driver.getLgse2().to_uint());
+        printf("DONE\n");
     }
 
     // Assign configuration values to be sent to the driver module
