ace3538bd1f99469111c7d50fa39d2e5f01f73c0
Clément Decoodt <clement@decoodt.eu>
2018-01-12 18:45:58 +0100

    Add get and send subcommands in main

--- tools/spi_cli/src/main.rs
+++ tools/spi_cli/src/main.rs
@@ -6,4 +6,12 @@ use clap::App;
 fn main() {
     let yaml_cli_config = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml_cli_config).get_matches();
+
+    if let Some(command_args) = matches.subcommand_matches("send") {
+        let command = command_args.value_of("command").unwrap();
+        println!("{:?}", command);
+    } else if let Some(command_args) = matches.subcommand_matches("get") {
+        let command = command_args.value_of("command").unwrap();
+        println!("{:?}", command);
+    }
 }
