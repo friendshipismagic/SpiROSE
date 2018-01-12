9f7fb4adcb9bf202048c00ebba02256deab53d81
Clément Decoodt <clement@decoodt.eu>
2018-01-12 17:42:02 +0100

    Add default CLI plugin

--- tools/spi_cli/src/main.rs
+++ tools/spi_cli/src/main.rs
@@ -1,3 +1,9 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("SPI ROSE Command Line Tool")
+        .version("0.1.0")
+        .about("Yet Another CLI for SPI matching the SpiROSE SPI behaviour")
+        .get_matches();
 }
