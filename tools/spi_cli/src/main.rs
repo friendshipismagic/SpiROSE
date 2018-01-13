cdf4835e8bf64d5ff6b62249b5b73077b5a485d4
Clément Decoodt <clement@decoodt.eu>
2018-01-13 18:53:19 +0100

    Add configure subcommand, use TOML for conf

--- tools/spi_cli/src/main.rs
+++ tools/spi_cli/src/main.rs
@@ -1,11 +1,36 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate serde_derive;
 extern crate spidev;
+extern crate toml;
 
 use std::io;
 use std::io::prelude::*;
+use std::fs::File;
 use clap::App;
 use spidev::{Spidev, SpidevOptions};
+use toml::Value;
+
+#[derive(Debug, Deserialize)]
+struct LEDDriverConfig {
+    lodvth: u8,
+    sel_td0: u8,
+    sel_gdly: bool,
+    xrefresh: bool,
+    sel_gck_edge: bool,
+    sel_pchg: bool,
+    espwm: bool,
+    lgse3: bool,
+    sel_sck_edge: bool,
+    lgse1: u8,
+    ccb: u16,
+    ccg: u16,
+    ccr: u16,
+    bc: u8,
+    poker_trans_mode: bool,
+    lgse2: u8,
+}
 
 #[derive(Debug)]
 struct SpiCommand {
@@ -26,8 +51,22 @@ fn main() {
     } else if let Some(command_args) = matches.subcommand_matches("get") {
         let command = command_args.value_of("command").unwrap();
         let decoded_command = decode_command(command).expect("Command not recognized");
-        send(&mut spi, &decoded_command, &vec![]);
+        get(&mut spi, &decoded_command, &vec![]);
+    } else if let Some(command_args) = matches.subcommand_matches("configure") {
+        let config_file_url = command_args.value_of("config_file").unwrap();
+        let mut config_file = File::open(config_file_url).expect("Configuration file not found");
+        let mut serialized_conf = String::new();
+        config_file
+            .read_to_string(&mut serialized_conf)
+            .expect("Unknown error when reading configuration file");
+        let led_config = serialized_conf.parse::<Value>().unwrap();
+        check_configuration(&led_config);
     }
+}
+
+fn check_configuration(led_config: &Value) -> io::Result<()> {
+    println!("{:?}", led_config);
+    Ok(())
 }
 
 fn create_spi() -> io::Result<Spidev> {
