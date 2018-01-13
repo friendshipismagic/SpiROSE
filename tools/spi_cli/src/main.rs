48bf6537ef1a2af03e7b5e4d70ef3f98dfe5ec5e
Clément Decoodt <clement@decoodt.eu>
2018-01-13 17:13:38 +0100

    Use RustFmt for code formating

--- tools/spi_cli/src/main.rs
+++ tools/spi_cli/src/main.rs
@@ -10,7 +10,7 @@ use spidev::{Spidev, SpidevOptions};
 #[derive(Debug)]
 struct SpiCommand {
     id: u8,
-    len: usize
+    len: usize,
 }
 
 fn main() {
@@ -21,13 +21,11 @@ fn main() {
 
     if let Some(command_args) = matches.subcommand_matches("send") {
         let command = command_args.value_of("command").unwrap();
-        let decoded_command = decode_command(&command)
-            .expect("Command not recognized");
+        let decoded_command = decode_command(&command).expect("Command not recognized");
         send(&mut spi, &decoded_command, None);
     } else if let Some(command_args) = matches.subcommand_matches("get") {
         let command = command_args.value_of("command").unwrap();
-        let decoded_command = decode_command(&command)
-            .expect("Command not recognized");
+        let decoded_command = decode_command(&command).expect("Command not recognized");
         send(&mut spi, &decoded_command, None);
     }
 }
@@ -47,15 +45,15 @@ fn create_spi() -> io::Result<Spidev> {
 
 fn decode_command(command: &str) -> Option<SpiCommand> {
     match command {
-        "blink_led" => Some(SpiCommand {id: 0xDE, len: 0}),
-        "rotation" => Some(SpiCommand {id: 0x4C, len: 1}),
-        "config" => Some(SpiCommand {id: 0xBF, len: 6}),
-        _ => None
+        "blink_led" => Some(SpiCommand { id: 0xDE, len: 0 }),
+        "rotation" => Some(SpiCommand { id: 0x4C, len: 1 }),
+        "config" => Some(SpiCommand { id: 0xBF, len: 6 }),
+        _ => None,
     }
 }
 
 fn send(spi: &mut Spidev, command: &SpiCommand, command_args: Option<&[u8]>) -> io::Result<()> {
-    spi.write(&vec!(command.id));
+    spi.write(&vec![command.id]);
 
     // Send optionnal arguments with the command (for instance, configuration data)
     if let Some(command_args) = command_args {
