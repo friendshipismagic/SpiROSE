bf5c0534d0b396c8befea74d84ba28933d8fa72e
Alexandre JANNIAUX <alexandre.janniaux@gmail.com>
2018-01-14 22:29:00 +0100

    FPGA/tb_src/systemc/tb_spi: multiple rotation send tests

--- FPGA/tb_src/systemc/tb_spi/monitor.cpp
+++ FPGA/tb_src/systemc/tb_spi/monitor.cpp
@@ -42,27 +42,44 @@ void Monitor::runTests() {
     for (int i = 0; i < 7; ++i) {
         sendCommand(0xA1 + i);
     }
+    sendCommand(0xA0);
 
+    // config is sent LSB order
     checkValueEq<48>(configOut.read(), 0xA6A5A4A3A2A1);
 
-    // Get rotation data
-    rotationData = 0xBEEF;
-    sendCommand(0xA0);
-    sendCommand(0x4C);
-
-    unsigned char bytes[2];
-    for (int i = 0; i < 2; ++i) {
-        sc_spawn(&bytes[i], sc_bind(&Monitor::captureValue, this));
+    rotationData = 0xB000;
+    for (int step = 0; step < 50; ++step) {
+        // Send blank command to add delay, nothing useful happens
         sendCommand(0xA0);
-    }
 
-    if (bytes[1] != 0xBE && bytes[0] != 0xEF) {
-        std::string msg;
-        msg += "Error during the process of get_rotation command, expected ";
-        msg += "0xBEEF, got 0xXXXX";
-        sprintf(&msg[msg.size() - 4], "%x%x", bytes[1], bytes[0]);
+        // Send "get rotation data" command
+        sendCommand(0x4C);
 
-        SC_REPORT_ERROR("spi", msg.c_str());
+        unsigned char bytes[2];
+        for (int byteNb = 0; byteNb < 2; ++byteNb) {
+            // Record each bytes of the returned value
+            sc_spawn(&bytes[byteNb], sc_bind(&Monitor::captureValue, this));
+            sendCommand(0xA0);
+        }
+
+        // expected values
+        unsigned char values[] = {
+            static_cast<unsigned char>(rotationData.read() & 0x00FF),
+            static_cast<unsigned char>((rotationData.read() & 0xFF00) >> 8),
+        };
+
+        if (bytes[1] != values[1] && bytes[0] != values[0]) {
+            std::stringstream msg;
+            msg << "Error during the process of get_rotation command, "
+                   "expected ";
+            msg << "0x" << std::hex << (int)values[1] << (int)values[0];
+            msg << ", got 0x";
+            msg << std::hex << (int)bytes[1] << (int)bytes[0];
+
+            SC_REPORT_ERROR("spi", msg.str().c_str());
+        }
+        rotationData = rotationData.read() + 1;
+        wait(clk.posedge_event());
     }
 
     sc_spawn(&capturedValue, sc_bind(&Monitor::captureValue, this));
