9d2e42a7e0de3eca9b36a02f2311532e25892801
alexis.bauvin <alexis.bauvin@telecom-paristech.fr>
2018-01-19 01:45:11 +0100

    framer: Load scene and count objects

--- tools/framer/src/main.cpp
+++ tools/framer/src/main.cpp
@@ -1,12 +1,33 @@
 #include <spirose/spirose.h>
 #include <iostream>
+#include <vector>
+
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <assimp/Importer.hpp>
 
 int main(int argc, char *argv[]) {
     glfwInit();
     GLFWwindow *window = spirose::createWindow(80, 48, 256);
     spirose::Context context(80, 48, 256);
 
-    // TODO
+    // Load mesh
+    Assimp::Importer importer;
+    // Ensure we only have triangles
+    importer.SetPropertyInteger(AI_CONFIG_PP_SBP_REMOVE,
+                                aiPrimitiveType_LINE | aiPrimitiveType_POINT);
+    const aiScene *scene = importer.ReadFile(
+        argv[1], aiProcess_JoinIdenticalVertices | aiProcess_Triangulate |
+                     aiProcess_SortByPType | aiProcess_PreTransformVertices);
+
+    if (!scene) {
+        std::cerr << "[ERR] Failed to open scene " << argv[1] << ": "
+                  << importer.GetErrorString() << std::endl;
+        exit(-1);
+    }
+
+    std::cout << "[INFO] Scene has " << scene->mNumMeshes << " meshes."
+              << std::endl;
 
     glfwDestroyWindow(window);
     glfwTerminate();
