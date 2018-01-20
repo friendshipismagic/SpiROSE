6744402cd59d2af8d49969e15fa8fa8eaf5315bc
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2018-01-20 22:38:06 +0100

    FPGA/src/sync_sig.sv: add signal synchronization module

---
+++ FPGA/src/sync_sig.sv
@@ -0,0 +1,22 @@
+module sync_sig #(
+    parameter RESET_VALUE=0
+)(
+    input nrst,
+    input clk,
+
+    input in_sig,
+    input out_sig
+);
+
+logic defer_sig;
+
+always @(posedge clk or negedge nrst)
+    if(~nrst) begin
+        defer_sig = RESET_VALUE;
+        out_sig <= RESET_VALUE;
+    end else begin
+        defer_sig <= in_sig;
+        out_sig <= defer_sig;
+    end
+
+endmodule
