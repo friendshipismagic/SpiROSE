b3f0e9c2dd2f03da609b2e01d1b2c03ee6809faa
Vincent CHARBONNIERAS <vcharbon@telecom-paristech.fr>
2018-01-22 00:13:05 +0100

    FPGA/quartus/rgb_pll: Remove pll reset, add test

--- FPGA/quartus/04_rgb_pll/rgb_pll.sv
+++ FPGA/quartus/04_rgb_pll/rgb_pll.sv
@@ -18,8 +18,34 @@ module rgb_pll
     output      pt_6
 );
 
-always_ff @(posedge pt_39)
-    pt_6 <= ~pt_6;
+logic clk33;
+assign pt_6 = clk33;
+
+logic clk33_shifted;
+
+logic locked;
+
+clock_33 main_clock_33 (
+    .inclk0(pt_39),
+    // Main 33 MHz clock
+    .c0(clk33),
+    /*
+     * Secondary 33 MHz clock phase being 90° shifted 
+     * compared to the main one above
+     */
+    .c1(clk33_shifted),
+    .locked(locked)
+);
+
+logic [32:0] counter;
+
+// Counter process that outputs a 1 kHz clock
+always_ff @(posedge clk33)
+    if (counter >= 33000) begin
+        counter <= 1;
+    end else begin
+        counter <= counter + 1;
+    end
 
 endmodule
 
