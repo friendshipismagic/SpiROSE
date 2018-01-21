d114da9f6e8002d46f837b96a8c4380510b84c50
Adrien Marcenat <adrien.marcenat@telecom-paristech.fr>
2018-01-21 19:45:09 +0100

    FPGA/quartus-de1soc/hall_sensor_test: remove dead code

--- FPGA/quartus-de1soc/hall_sensor_test/DE1_SoC.sv
+++ FPGA/quartus-de1soc/hall_sensor_test/DE1_SoC.sv
@@ -5,25 +5,25 @@
 
 module DE1_SoC(
       ///////// ADC /////////
-      inout  wire        adc_cs_n,
+      inout  logic        adc_cs_n,
       output logic       adc_din,
-      input  wire        adc_dout,
+      input  logic        adc_dout,
       output logic       adc_sclk,
       ///////// AUD /////////
-      input  wire        aud_adcdat,
-      inout  wire        aud_adclrck,
-      inout  wire        aud_bclk,
+      input  logic        aud_adcdat,
+      inout  logic        aud_adclrck,
+      inout  logic        aud_bclk,
       output logic       aud_dacdat,
-      inout  wire        aud_daclrck,
+      inout  logic        aud_daclrck,
       output logic       aud_xck,
       ///////// CLOCK2 /////////
-      input  wire        clock2_50,
+      input  logic        clock2_50,
       ///////// CLOCK3 /////////
-      input  wire        clock3_50,
+      input  logic        clock3_50,
       ///////// CLOCK4 /////////
-      input  wire        clock4_50,
+      input  logic        clock4_50,
       ///////// CLOCK /////////
-      input  wire        clock_50,
+      input  logic        clock_50,
       ///////// DRAM /////////
       output logic[12:0] dram_addr,
       output logic[1:0]  dram_ba,
@@ -31,7 +31,7 @@ module DE1_SoC(
       output logic       dram_cke,
       output logic       dram_clk,
       output logic       dram_cs_n,
-      inout  wire [15:0] dram_dq,
+      inout  logic [15:0] dram_dq,
       output logic       dram_ldqm,
       output logic       dram_ras_n,
       output logic       dram_udqm,
@@ -40,10 +40,10 @@ module DE1_SoC(
       output logic       fan_ctrl,
       ///////// FPGA /////////
       output logic       fpga_i2c_sclk,
-      inout  wire        fpga_i2c_sdat,
+      inout  logic        fpga_i2c_sdat,
       ///////// GPIO /////////
-      inout  wire [35:0] gpio_0,
-      inout  wire [35:0] gpio_1,
+      inout  logic [35:0] gpio_0,
+      inout  logic [35:0] gpio_1,
       ///////// hex0 /////////
       output logic[6:0]  hex0,
       ///////// hex1 /////////
@@ -59,7 +59,7 @@ module DE1_SoC(
 
 `ifdef ENABLE_HPS
       ///////// HPS /////////
-      inout  wire        hps_conv_usb_n,
+      inout  logic        hps_conv_usb_n,
       output logic[14:0] hps_ddr3_addr,
       output logic[2:0]  hps_ddr3_ba,
       output logic       hps_ddr3_cas_n,
@@ -68,70 +68,70 @@ module DE1_SoC(
       output logic       hps_ddr3_ck_p,
       output logic       hps_ddr3_cs_n,
       output logic[3:0]  hps_ddr3_dm,
-      inout  wire [31:0] hps_ddr3_dq,
-      inout  wire [3:0]  hps_ddr3_dqs_n,
-      inout  wire [3:0]  hps_ddr3_dqs_p,
+      inout  logic [31:0] hps_ddr3_dq,
+      inout  logic [3:0]  hps_ddr3_dqs_n,
+      inout  logic [3:0]  hps_ddr3_dqs_p,
       output logic       hps_ddr3_odt,
       output logic       hps_ddr3_ras_n,
       output logic       hps_ddr3_reset_n,
-      input  wire        hps_ddr3_rzq,
+      input  logic        hps_ddr3_rzq,
       output logic       hps_ddr3_we_n,
       output logic       hps_enet_gtx_clk,
-      inout  wire        hps_enet_int_n,
+      inout  logic        hps_enet_int_n,
       output logic       hps_enet_mdc,
-      inout  wire        hps_enet_mdio,
-      input  wire        hps_enet_rx_clk,
-      input  wire [3:0]  hps_enet_rx_data,
-      input  wire        hps_enet_rx_dv,
+      inout  logic        hps_enet_mdio,
+      input  logic        hps_enet_rx_clk,
+      input  logic [3:0]  hps_enet_rx_data,
+      input  logic        hps_enet_rx_dv,
       output logic[3:0]  hps_enet_tx_data,
       output logic       hps_enet_tx_en,
-      inout  wire [3:0]  hps_flash_data,
+      inout  logic [3:0]  hps_flash_data,
       output logic       hps_flash_dclk,
       output logic       hps_flash_ncso,
-      inout  wire        hps_gsensor_int,
-      inout  wire        hps_i2c1_sclk,
-      inout  wire        hps_i2c1_sdat,
-      inout  wire        hps_i2c2_sclk,
-      inout  wire        hps_i2c2_sdat,
-      inout  wire        hps_i2c_control,
-      inout  wire        hps_key,
-      inout  wire        hps_led,
-      inout  wire        hps_ltc_gpio,
+      inout  logic        hps_gsensor_int,
+      inout  logic        hps_i2c1_sclk,
+      inout  logic        hps_i2c1_sdat,
+      inout  logic        hps_i2c2_sclk,
+      inout  logic        hps_i2c2_sdat,
+      inout  logic        hps_i2c_control,
+      inout  logic        hps_key,
+      inout  logic        hps_led,
+      inout  logic        hps_ltc_gpio,
       output logic       hps_sd_clk,
-      inout  wire        hps_sd_cmd,
-      inout  wire [3:0]  hps_sd_data,
+      inout  logic        hps_sd_cmd,
+      inout  logic [3:0]  hps_sd_data,
       output logic       hps_spim_clk,
-      input  wire        hps_spim_miso,
+      input  logic        hps_spim_miso,
       output logic       hps_spim_mosi,
-      inout  wire        hps_spim_ss,
-      input  wire        hps_uart_rx,
+      inout  logic        hps_spim_ss,
+      input  logic        hps_uart_rx,
       output logic       hps_uart_tx,
-      input  wire        hps_usb_clkout,
-      inout  wire [7:0]  hps_usb_data,
-      input  wire        hps_usb_dir,
-      input  wire        hps_usb_nxt,
+      input  logic        hps_usb_clkout,
+      inout  logic [7:0]  hps_usb_data,
+      input  logic        hps_usb_dir,
+      input  logic        hps_usb_nxt,
       output logic       hps_usb_stp,
 `endif /*ENABLE_HPS*/
       ///////// IRDA /////////
-      input  wire        irda_rxd,
+      input  logic        irda_rxd,
       output logic       irda_txd,
       ///////// key /////////
-      input  wire [3:0]  key,
+      input  logic [3:0]  key,
       ///////// ledr /////////
       output logic[9:0]  ledr,
       ///////// PS2 /////////
-      inout  wire        ps2_clk,
-      inout  wire        ps2_clk2,
-      inout  wire        ps2_dat,
-      inout  wire        ps2_dat2,
+      inout  logic        ps2_clk,
+      inout  logic        ps2_clk2,
+      inout  logic        ps2_dat,
+      inout  logic        ps2_dat2,
       ///////// sw /////////
-      input  wire [9:0]  sw,
+      input  logic [9:0]  sw,
       ///////// TD /////////
-      input  wire        td_clk27,
-      input  wire[7:0]   td_data,
-      input  wire        td_hs,
+      input  logic        td_clk27,
+      input  logic[7:0]   td_data,
+      input  logic        td_hs,
       output logic       td_reset_n,
-      input  wire        td_vs,
+      input  logic        td_vs,
       ///////// VGA /////////
       output logic[7:0]  vga_b,
       output logic       vga_blank_n,
@@ -143,41 +143,6 @@ module DE1_SoC(
       output logic       vga_vs
 );
 
-//    Turn off all display     //////////////////////////////////////
-//assign    hex0        =    'h7F;
-//assign    hex1        =    'h7F;
-//assign    hex2        =    'h7F;
-//assign    hex3        =    'h7F;
-//assign    hex4        =    'h7F;
-//assign    hex5        =    'h7F;
-
-/////////////////////////////////////////////////////////////////////
-// Sorties video VGA     ////////////////////////////////////////////
-assign  vga_clk       =  '0;       // Horloge du CNA (DAC)
-assign  vga_hs        =  '0;       // Synchro horizontale
-assign  vga_vs        =  '0;       // Synchro verticale
-assign  vga_blank_n   =  '0;       // Autoriser l'affichage
-assign  vga_sync_n    =  '0;       // non utilisé, doit resté à zéro
-assign  vga_r         =  '1;       // 10bits de Rouge
-assign  vga_g         =  '1;       // 10bits de Vert
-assign  vga_b         =  '1;       // 10bits de Bleu
-//////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////
-//    Tous les ports en entrees/sorties mis au 3e etat       /////////
-//////////////////////////////////////////////////////////////////////
-assign gpio_0        = 'z;
-assign gpio_1        = 'z;
-assign adc_cs_n      = 'z;
-assign dram_dq       = 'z;
-assign fpga_i2c_sdat = 'z;
-assign ps2_clk       = 'z;
-assign ps2_clk2      = 'z;
-assign ps2_dat       = 'z;
-assign ps2_dat2      = 'z;
-//   Les sorties non utilisées seront mise à zéro
-
 logic hall_1;
 logic hall_2;
 logic nrst;
@@ -188,7 +153,7 @@ logic [3:0] digit_1;
 logic [3:0] digit_2;
 
 // 66 MHz clock generator
-wire clock_66, lock;
+logic clock_66, lock;
 clk_66 main_clk_66 (
 	.refclk(clock_50),
 	.rst(sw[0]),
@@ -210,37 +175,34 @@ assign hall_2 = key[2];
 assign digit_0 = slice_cnt % 10;
 assign digit_1 = ((slice_cnt % 100) - digit_0)/10;
 assign digit_2 = (slice_cnt - digit_0 - digit_1)/100;
-//assign digit_0 = slice_cnt & 4'hf;
-//assign digit_1 = (slice_cnt >> 4) & 4'hf;
-
 
 // Heartbeat LED 66MHz
 logic[24:0] heartbeat_counter_66;
 always_ff @(posedge clock_66 or negedge nrst)
-	if(~nrst) begin
-		ledr[0] <= '0;
-		heartbeat_counter_66 <= '0;
-	end else begin
-		heartbeat_counter_66 <= heartbeat_counter_66 + 1'b1;
-		if(heartbeat_counter_66 == 30_000_000) begin
-			ledr[0] <= ~ledr[0];
-			heartbeat_counter_66 <= '0;
-		end
-	end
+    if(~nrst) begin
+        ledr[0] <= '0;
+        heartbeat_counter_66 <= '0;
+    end else begin
+        heartbeat_counter_66 <= heartbeat_counter_66 + 1'b1;
+        if(heartbeat_counter_66 == 30_000_000) begin
+            ledr[0] <= ~ledr[0];
+            heartbeat_counter_66 <= '0;
+        end
+    end
 
 // Heartbeat LED 33MHz
 logic[24:0] heartbeat_counter_33;
 always_ff @(posedge clock_33 or negedge nrst)
-	if(~nrst) begin
-		ledr[1] <= '0;
-		heartbeat_counter_33 <= '0;
-	end else begin
-		heartbeat_counter_33 <= heartbeat_counter_33 + 1'b1;
-		if(heartbeat_counter_33 == 30_000_000) begin
-			ledr[1] <= ~ledr[1];
-			heartbeat_counter_33 <= '0;
-		end
-	end
+    if(~nrst) begin
+        ledr[1] <= '0;
+        heartbeat_counter_33 <= '0;
+    end else begin
+        heartbeat_counter_33 <= heartbeat_counter_33 + 1'b1;
+        if(heartbeat_counter_33 == 30_000_000) begin
+            ledr[1] <= ~ledr[1];
+            heartbeat_counter_33 <= '0;
+        end
+    end
 
 // Test for hall effect sensors
 hall_sensor main_hall_sensor (
@@ -268,13 +230,13 @@ SEG7_LUT lut2 (
 );
 
 always_ff @(posedge clock_33 or negedge nrst) begin
-	if (~nrst) begin
-		ledr[8] <= '0;
-	end else begin
-		if (position_sync) begin
-			ledr[8] <= ~ledr[8];
-		end
-	end
+    if (~nrst) begin
+        ledr[8] <= '0;
+    end else begin
+        if (position_sync) begin
+            ledr[8] <= ~ledr[8];
+        end
+    end
 end
 
 
