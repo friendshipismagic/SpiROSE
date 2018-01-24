d6a5505337266a30fd89bc3b7807733cd5af6f30
Vincent CHARBONNIERAS <vcharbon@telecom-paristech.fr>
2018-01-25 00:44:30 +0100

    FPGA/quartus/hall_sensor: Use of the topmodule template, assignment of *_a and *_b singals altogether

--- FPGA/quartus/09_hall_sensor/top.sv
+++ FPGA/quartus/09_hall_sensor/top.sv
@@ -1,13 +1,51 @@
-module hall_sensor_test
+module Top
 (
-    input          rgb_clk,
-    input  [1:0]   hall,
+ // RGB
+ input logic         rgb_clk,
+ input logic         rgb_clk2,
+ input logic         rgb_hsync,
+ input logic         rgb_vsync,
+ input logic [23:0]  rgb_d,
 
-    output [29:0]  drv_sin,
-    output         drv_sclk_a,
-    output         drv_gclk_a,
-    output         drv_lat_a,
-    output [7:0]   fpga_mul_a
+ // LVDS
+ input logic [3:0]   lvds_tx_p,
+ input logic [3:0]   lvds_tx_n,
+ input logic         lvds_clk_p,
+ input logic         lvds_clk_n,
+
+ // Drivers
+ output logic        drv_gclk_a,
+ output logic        drv_gclk_b,
+ output logic        drv_sclk_a,
+ output logic        drv_sclk_b,
+ output logic        drv_lat_a,
+ output logic        drv_lat_b,
+ output logic [29:0] drv_sin,
+ output logic [7:0]   fpga_mul_a,
+ output logic [7:0]   fpga_mul_b,
+
+ // SPI
+ input logic         som_cs,
+ input logic         som_sclk,
+ input logic         som_mosi,
+ output logic        som_miso,
+
+ // Hall sensors
+ input logic [1:0]   hall,
+
+ // Encoder
+ input logic         encoder_A,
+ input logic         encoder_B,
+ input logic         encoder_C,
+ input logic         encoder_D,
+
+ // Test points
+ output logic        pt_6,
+ input logic         pt_39,
+ output logic        pt_23,
+ output logic        pt_24,
+ output logic        pt_26,
+ output logic        pt_27
 );
 
 logic clk;
@@ -102,6 +140,11 @@ always_ff @(posedge clk or negedge nrst)
             7: fpga_mul_a <= 8'b0100_0000;
         endcase
     end
+
+assign drv_sclk_b = drv_sclk_a;
+assign drv_gclk_b = drv_gclk_a;
+assign drv_lat_b = drv_lat_a;
+assign fpga_mul_b = fpga_mul_a;
 
 endmodule
 
