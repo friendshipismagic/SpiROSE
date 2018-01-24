e1e5e0b860dc25d80d01f7629c663f7126e11c4b
Alexis Polti <alexis.polti@telecom-paristech.fr>
2018-01-24 20:25:16 +0100

    Correct top module and add timing assignments

--- FPGA/quartus/04_rgb_pll/rgb_pll.sv
+++ FPGA/quartus/04_rgb_pll/rgb_pll.sv
@@ -1,25 +1,51 @@
-// Copyright (C) 1991-2014 Altera Corporation
-// Your use of Altera Corporation's design tools, logic functions
-// and other software and tools, and its AMPP partner logic
-// functions, and any output files from any of the foregoing
-// (including device programming or simulation files), and any
-// associated documentation or information are expressly subject
-// to the terms and conditions of the Altera Program License
-// Subscription Agreement, Altera MegaCore Function License
-// Agreement, or other applicable license agreement, including,
-// without limitation, that your use is for the sole purpose of
-// programming logic devices manufactured by Altera and sold by
-// Altera or its authorized distributors.  Please refer to the
-// applicable agreement for further details.
-
-module rgb_pll
+module Top
 (
-    input       rgb_clk,
-    input       rgb_clk2,
-    output      pt_6,
-    output      pt_26,
-    output      pt_23,
-    output      pt_27
+ // RGB
+ input logic         rgb_clk,
+ input logic         rgb_clk2,
+ input logic         rgb_hsync,
+ input logic         rgb_vsync,
+ input logic [23:0]  rgb_d,
+
+ // LVDS
+ input logic [3:0]   lvds_tx_p,
+ input logic [3:0]   lvds_tx_n,
+ input logic         lvds_clk_p,
+ input logic         lvds_clk_n,
+
+ // Drivers
+ output logic        drv_gclk_a,
+ output logic        drv_gclk_b,
+ output logic        drv_sclk_a,
+ output logic        drv_sclk_b,
+ output logic        drv_lat_a,
+ output logic        drv_lat_b,
+ output logic [29:0] drv_sin,
+ input logic [7:0]   fpga_mul_a,
+ input logic [7:0]   fpga_mul_b,
+
+ // SPI
+ input logic         som_cs,
+ input logic         som_sclk,
+ input logic         som_mosi,
+ output logic        som_miso,
+
+ // Hall sensors
+ input logic [1:0]   hall,
+
+ // Encoder
+ input logic         encoder_A,
+ input logic         encoder_B,
+ input logic         encoder_C,
+ input logic         encoder_D,
+
+ // Test points
+ output logic        pt_6,
+ input logic         pt_39,
+ output logic        pt_23,
+ output logic        pt_24,
+ output logic        pt_26,
+ output logic        pt_27
 );
 
 logic clk66;
