88a5aefc6a7771ee74a8111b0008de7f9766ec78
Alexandre Janniaux <alexandre.janniaux@gmail.com>
2018-01-24 14:02:38 +0100

    FPGA/src/heartbeat: add heartbeat module

---
+++ FPGA/src/heartbeat.sv
@@ -0,0 +1,26 @@
+module heartbeat #(parameter COUNTER=0) (
+    input clk,
+    input nrst,
+    output toggle
+);
+
+logic [$clog2(COUNTER)+1:0] counter;
+
+logic state;
+assign toggle = state;
+
+always @(posedge clk or negedge nrst)
+    if (~nrst) begin
+        state <= '0;
+        counter <= '0;
+    end else begin
+        if (counter == COUNTER-1) begin
+            state <= ~state;
+            counter <= '0;
+        end else begin
+            counter <= counter + 1'b1;
+        end
+    end
+
+
+endmodule
