c8bbecebdd37b0d8b972d86d8c92ba8979aaf935
Samuel Tardieu <sam@rfc1149.net>
2018-01-30 23:37:44 +0100

    Implement send_image command

--- tools/spi_cli/src/framebuffer.rs
+++ tools/spi_cli/src/framebuffer.rs
@@ -1,12 +1,12 @@
-use image::{GenericImage, ImageBuffer};
+use image::{DynamicImage, GenericImage, Pixel as ImgPixel};
 
 use super::*;
 
-static ReadPixel: SpiCommand = SpiCommand {
+static READ_PIXEL: SpiCommand = SpiCommand {
     id: 0x20,
     recv_len: 3,
 };
-static WritePixel: SpiCommand = SpiCommand {
+static WRITE_PIXEL: SpiCommand = SpiCommand {
     id: 0x21,
     recv_len: 0,
 };
@@ -18,12 +18,22 @@ pub struct Pixel {
     pub b: u8,
 }
 
+impl From<image::Rgb<u8>> for Pixel {
+    fn from(d: image::Rgb<u8>) -> Pixel {
+        Pixel {
+            r: d.data[0],
+            g: d.data[1],
+            b: d.data[2],
+        }
+    }
+}
+
 fn manage(spi: &mut Spidev, verbose: bool, dummy: bool) -> errors::Result<()> {
-    transfer(spi, &Manage, &[], verbose, dummy).map(|_| ())
+    transfer(spi, &MANAGE, &[], verbose, dummy).map(|_| ())
 }
 
 fn release(spi: &mut Spidev, verbose: bool, dummy: bool) -> errors::Result<()> {
-    transfer(spi, &Release, &[], verbose, dummy).map(|_| ())
+    transfer(spi, &RELEASE, &[], verbose, dummy).map(|_| ())
 }
 
 pub fn read_pixel(
@@ -47,7 +57,7 @@ fn read_single_pixel(
     dummy: bool,
 ) -> errors::Result<Pixel> {
     let (block, offset) = block_offset(x, y);
-    let answer = transfer(spi, &ReadPixel, &[block, offset], verbose, dummy)?;
+    let answer = transfer(spi, &READ_PIXEL, &[block, offset], verbose, dummy)?;
     Ok(Pixel {
         r: answer[2],
         g: answer[1],
@@ -89,11 +99,33 @@ fn write_single_pixel(
     let (block, offset) = block_offset(x, y);
     transfer(
         spi,
-        &WritePixel,
+        &WRITE_PIXEL,
         &[block, offset, p.b, p.g, p.r],
         verbose,
         dummy,
     ).map(|_| ())
+}
+
+pub fn send_image(
+    spi: &mut Spidev,
+    img: &DynamicImage,
+    verbose: bool,
+    dummy: bool,
+) -> errors::Result<()> {
+    if img.dimensions() != (40, 48) {
+        return Err(format!(
+            "Image dimension should be (40, 48), but is {:?} instead",
+            img.dimensions()
+        ).into());
+    }
+    manage(spi, verbose, dummy)?;
+    for x in 0..40 {
+        for y in 0..48 {
+            let p = img.get_pixel(x, y).to_rgb().into();
+            write_single_pixel(spi, x as u8, y as u8, &p, verbose, dummy)?;
+        }
+    }
+    release(spi, verbose, dummy)
 }
 
 fn block_offset(x: u8, y: u8) -> (u8, u8) {
