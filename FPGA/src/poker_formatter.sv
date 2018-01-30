c344054204b12a399f5ef8567148a69c7c36cca1
Clément Decoodt <clement@decoodt.eu>
2018-01-30 19:27:43 +0100

    Add right poker formatter

--- FPGA/src/poker_formatter.sv
+++ FPGA/src/poker_formatter.sv
@@ -1,20 +1,18 @@
+`default_nettype none
 module poker_formatter (
     input [383:0]  data_in,
     output [431:0] data_out
 );
 
 always_comb begin
-    for(int led=0; led<16; led++) begin
-        for(int poker=8; poker>0; --poker) begin
-            data_out[poker*48 - 3*(16-led)+:3] = {
-                data_in[led*24 + poker - 1],
-                data_in[led*24 + 8 + poker - 1],
-                data_in[led*24 + 16 + poker - 1]
-            };
-        end
-    end
-    data_out[47:0] = '0;
+   data_out = '0;
+   for(int bit_idx=1; bit_idx<9; bit_idx++) begin
+      for(int led=0; led<16; led++) begin
+         data_out[48*bit_idx + 3*led + 0] = data_in[24*led + (bit_idx - 1) + 0];
+         data_out[48*bit_idx + 3*led + 1] = data_in[24*led + (bit_idx - 1) + 8];
+         data_out[48*bit_idx + 3*led + 2] = data_in[24*led + (bit_idx - 1) + 16];
+      end
+   end
 end
-
 
 endmodule
