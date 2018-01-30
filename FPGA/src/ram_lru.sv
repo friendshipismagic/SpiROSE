844de13b9e9ac06485fe2da70cb17839fb5d0b55
alexis.bauvin <alexis.bauvin@telecom-paristech.fr>
2018-01-30 23:37:25 +0100

    ram_lru: Replace tabs with spaces

--- FPGA/src/ram_lru.sv
+++ FPGA/src/ram_lru.sv
@@ -34,50 +34,50 @@ assign read_data = internal_read_data[bypass_lru ? write_ram : read_ram];
 
 // RAM blocks instanciation
 ram ram0 (
-	.clock(clk),
-	.wren(internal_write_ena[0]),
-	.data(write_data), .wraddress(write_addr),
-	.q(internal_read_data[0]), .rdaddress(read_addr)
+    .clock(clk),
+    .wren(internal_write_ena[0]),
+    .data(write_data), .wraddress(write_addr),
+    .q(internal_read_data[0]), .rdaddress(read_addr)
 );
 ram ram1 (
-	.clock(clk),
-	.wren(internal_write_ena[1]),
-	.data(write_data), .wraddress(write_addr),
-	.q(internal_read_data[1]), .rdaddress(read_addr)
+    .clock(clk),
+    .wren(internal_write_ena[1]),
+    .data(write_data), .wraddress(write_addr),
+    .q(internal_read_data[1]), .rdaddress(read_addr)
 );
 ram ram2 (
-	.clock(clk),
-	.wren(internal_write_ena[2]),
-	.data(write_data), .wraddress(write_addr),
-	.q(internal_read_data[2]), .rdaddress(read_addr)
+    .clock(clk),
+    .wren(internal_write_ena[2]),
+    .data(write_data), .wraddress(write_addr),
+    .q(internal_read_data[2]), .rdaddress(read_addr)
 );
 
 // RAM switching logic
 logic old_read_done, old_write_done;
 always_ff @(posedge clk or negedge nrst)
-	if (~nrst) begin
-		write_ram <= 0;
-		read_ram <= 1;
-		free_ram <= 2;
-		old_read_done <= old_write_done <= 0;
-	end else begin
-		old_read_done <= read_done;
-		old_write_done <= write_done;
+    if (~nrst) begin
+        write_ram <= 0;
+        read_ram <= 1;
+        free_ram <= 2;
+        old_read_done <= old_write_done <= 0;
+    end else begin
+        old_read_done <= read_done;
+        old_write_done <= write_done;
 
-		if (~old_read_done && read_done && ~old_write_done && write_done) begin
-			// When both finished at the same time, the free RAM is outdated.o
-			// Thus, we only need to swap the read and write RAM
-			read_ram <= write_ram;
-			write_ram <= read_ram;
-		end else if (~old_read_done && read_done) begin
-			// Swap free buffer and read buffer
-			read_ram <= free_ram;
-			free_ram <= read_ram;
-		end else if (~old_write_done && write_done) begin
-			// Swap free buffer and write buffer
-			write_ram <= free_ram;
-			free_ram <= write_ram;
-		end
-	end
+        if (~old_read_done && read_done && ~old_write_done && write_done) begin
+            // When both finished at the same time, the free RAM is outdated.o
+            // Thus, we only need to swap the read and write RAM
+            read_ram <= write_ram;
+            write_ram <= read_ram;
+        end else if (~old_read_done && read_done) begin
+            // Swap free buffer and read buffer
+            read_ram <= free_ram;
+            free_ram <= read_ram;
+        end else if (~old_write_done && write_done) begin
+            // Swap free buffer and write buffer
+            write_ram <= free_ram;
+            free_ram <= write_ram;
+        end
+    end
 
 endmodule
