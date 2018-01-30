7c7ead81aeb95897e8b79505612cd517f4791b9e
alexis.bauvin <alexis.bauvin@telecom-paristech.fr>
2018-01-30 22:15:26 +0100

    Add the ram_lru module
    It makes very few assumptions, only that the write_addr starts at 0

--- FPGA/src/ram_lru.sv
+++ FPGA/src/ram_lru.sv
@@ -1,218 +1,106 @@
 `default_nettype none
 module ram_lru (
-    input clk,
-    input nrst,
+    input logic clk,
+    input logic nrst,
 
     // Video stream input
-    input [31:0] write_addr,
-    input [23:0] write_data,
-    input write_enab,
+    input logic [31:0] write_addr,
+    input logic [23:0] write_data,
+    input logic        write_enab,
 
     // Ram access
-    input  [31:0] ub0d0_ram_addr,
-    input  [31:0] ub0d1_ram_addr,
-    input  [31:0] ub0d2_ram_addr,
-    input  [31:0] ub1d0_ram_addr,
-    input  [31:0] ub1d1_ram_addr,
-    input  [31:0] ub1d2_ram_addr,
-    input  [31:0] ub2d0_ram_addr,
-    input  [31:0] ub2d1_ram_addr,
-    input  [31:0] ub2d2_ram_addr,
-    input  [31:0] ub3d0_ram_addr,
-    input  [31:0] ub3d1_ram_addr,
-    input  [31:0] ub3d2_ram_addr,
-    input  [31:0] ub4d0_ram_addr,
-    input  [31:0] ub4d1_ram_addr,
-    input  [31:0] ub4d2_ram_addr,
-
-    output [23:0] ub0d0_ram_data,
-    output [23:0] ub0d1_ram_data,
-    output [23:0] ub0d2_ram_data,
-    output [23:0] ub1d0_ram_data,
-    output [23:0] ub1d1_ram_data,
-    output [23:0] ub1d2_ram_data,
-    output [23:0] ub2d0_ram_data,
-    output [23:0] ub2d1_ram_data,
-    output [23:0] ub2d2_ram_data,
-    output [23:0] ub3d0_ram_data,
-    output [23:0] ub3d1_ram_data,
-    output [23:0] ub3d2_ram_data,
-    output [23:0] ub4d0_ram_data,
-    output [23:0] ub4d1_ram_data,
-    output [23:0] ub4d2_ram_data,
+    input logic  [6:0] read_addr,
+    input logic [23:0] read_data,
+    input logic        read_done
 );
 
+// RAM size in 24 bit words
+localparam RAM_SIZE = 8 * 16;
+
+// MUX signals to discriminate the read and write ram. Those two should always
+// be different
+logic [2:0] write_ram;
+logic [2:0]  read_ram;
+// This is the next ram to use (i.e. the least recently used)
+logic [2:0]  free_ram;
+
 // Internal RAM signals
-logic [23:0] ub0d0_internal_ram_data [2:0];
-logic [23:0] ub0d1_internal_ram_data [2:0];
-logic [23:0] ub0d2_internal_ram_data [2:0];
-logic [23:0] ub1d0_internal_ram_data [2:0];
-logic [23:0] ub1d1_internal_ram_data [2:0];
-logic [23:0] ub1d2_internal_ram_data [2:0];
-logic [23:0] ub2d0_internal_ram_data [2:0];
-logic [23:0] ub2d1_internal_ram_data [2:0];
-logic [23:0] ub2d2_internal_ram_data [2:0];
-logic [23:0] ub3d0_internal_ram_data [2:0];
-logic [23:0] ub3d1_internal_ram_data [2:0];
-logic [23:0] ub3d2_internal_ram_data [2:0];
-logic [23:0] ub4d0_internal_ram_data [2:0];
-logic [23:0] ub4d1_internal_ram_data [2:0];
-logic [23:0] ub4d2_internal_ram_data [2:0];
-logic        ub0d0_internal_ram_write_enab [2:0];
-logic        ub0d1_internal_ram_write_enab [2:0];
-logic        ub0d1_internal_ram_write_enab [2:0];
-logic        ub1d0_internal_ram_write_enab [2:0];
-logic        ub1d1_internal_ram_write_enab [2:0];
-logic        ub1d2_internal_ram_write_enab [2:0];
-logic        ub2d0_internal_ram_write_enab [2:0];
-logic        ub2d1_internal_ram_write_enab [2:0];
-logic        ub2d2_internal_ram_write_enab [2:0];
-logic        ub3d0_internal_ram_write_enab [2:0];
-logic        ub3d1_internal_ram_write_enab [2:0];
-logic        ub3d2_internal_ram_write_enab [2:0];
-logic        ub4d0_internal_ram_write_enab [2:0];
-logic        ub4d1_internal_ram_write_enab [2:0];
-logic        ub4d2_internal_ram_write_enab [2:0];
+logic  [6:0] internal_write_addr;
+logic [23:0] internal_write_data;
+logic        internal_write_ena [2:0];
+wire  [23:0] internal_read_data [2:0];
 
-// RAM blocks instanciation 
-ram ub0d0_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d0_internal_ram_write_enab[0]),
-   .q(ub0d0_internal_ram_data[0]), .rdaddress(ub0d0_ram_addr));
-ram ub0d0_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d0_internal_ram_write_enab[1]),
-   .q(ub0d0_internal_ram_data[1]), .rdaddress(ub0d0_ram_addr));
-ram ub0d0_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d0_internal_ram_write_enab[2]),
-   .q(ub0d0_internal_ram_data[2]), .rdaddress(ub0d0_ram_addr));
-ram ub0d1_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d1_internal_ram_write_enab[0]),
-   .q(ub0d1_internal_ram_data[0]), .rdaddress(ub0d1_ram_addr));
-ram ub0d1_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d1_internal_ram_write_enab[1]),
-   .q(ub0d1_internal_ram_data[1]), .rdaddress(ub0d1_ram_addr));
-ram ub0d1_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d1_internal_ram_write_enab[2]),
-   .q(ub0d1_internal_ram_data[2]), .rdaddress(ub0d1_ram_addr));
-ram ub0d2_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d2_internal_ram_write_enab[0]),
-   .q(ub0d2_internal_ram_data[0]), .rdaddress(ub0d2_ram_addr));
-ram ub0d2_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d2_internal_ram_write_enab[1]),
-   .q(ub0d2_internal_ram_data[1]), .rdaddress(ub0d2_ram_addr));
-ram ub0d2_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub0d2_internal_ram_write_enab[2]),
-   .q(ub0d2_internal_ram_data[2]), .rdaddress(ub0d2_ram_addr));
+// Whenever the write to a ram has been done
+wire  write_done;
 
-ram ub1d0_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d0_internal_ram_write_enab[0]),
-   .q(ub1d0_internal_ram_data[0]), .rdaddress(ub1d0_ram_addr));
-ram ub1d0_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d0_internal_ram_write_enab[1]),
-   .q(ub1d0_internal_ram_data[1]), .rdaddress(ub1d0_ram_addr));
-ram ub1d0_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d0_internal_ram_write_enab[2]),
-   .q(ub1d0_internal_ram_data[2]), .rdaddress(ub1d0_ram_addr));
-ram ub1d1_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d1_internal_ram_write_enab[0]),
-   .q(ub1d1_internal_ram_data[0]), .rdaddress(ub1d1_ram_addr));
-ram ub1d1_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d1_internal_ram_write_enab[1]),
-   .q(ub1d1_internal_ram_data[1]), .rdaddress(ub1d1_ram_addr));
-ram ub1d1_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d1_internal_ram_write_enab[2]),
-   .q(ub1d1_internal_ram_data[2]), .rdaddress(ub1d1_ram_addr));
-ram ub1d2_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d2_internal_ram_write_enab[0]),
-   .q(ub1d2_internal_ram_data[0]), .rdaddress(ub1d2_ram_addr));
-ram ub1d2_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d2_internal_ram_write_enab[1]),
-   .q(ub1d2_internal_ram_data[1]), .rdaddress(ub1d2_ram_addr));
-ram ub1d2_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub1d2_internal_ram_write_enab[2]),
-   .q(ub1d2_internal_ram_data[2]), .rdaddress(ub1d2_ram_addr));
+// Route signals to the proper RAMs
+assign read_data = internal_read_data[read_ram];
 
-ram ub2d0_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d0_internal_ram_write_enab[0]),
-   .q(ub2d0_internal_ram_data[0]), .rdaddress(ub2d0_ram_addr));
-ram ub2d0_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d0_internal_ram_write_enab[1]),
-   .q(ub2d0_internal_ram_data[1]), .rdaddress(ub2d0_ram_addr));
-ram ub2d0_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d0_internal_ram_write_enab[2]),
-   .q(ub2d0_internal_ram_data[2]), .rdaddress(ub2d0_ram_addr));
-ram ub2d1_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d1_internal_ram_write_enab[0]),
-   .q(ub2d1_internal_ram_data[0]), .rdaddress(ub2d1_ram_addr));
-ram ub2d1_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d1_internal_ram_write_enab[1]),
-   .q(ub2d1_internal_ram_data[1]), .rdaddress(ub2d1_ram_addr));
-ram ub2d1_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d1_internal_ram_write_enab[2]),
-   .q(ub2d1_internal_ram_data[2]), .rdaddress(ub2d1_ram_addr));
-ram ub2d2_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d2_internal_ram_write_enab[0]),
-   .q(ub2d2_internal_ram_data[0]), .rdaddress(ub2d2_ram_addr));
-ram ub2d2_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d2_internal_ram_write_enab[1]),
-   .q(ub2d2_internal_ram_data[1]), .rdaddress(ub2d2_ram_addr));
-ram ub2d2_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub2d2_internal_ram_write_enab[2]),
-   .q(ub2d2_internal_ram_data[2]), .rdaddress(ub2d2_ram_addr));
+// RAM blocks instanciation
+ram ram0 (
+	.clock(clk),
+	.wren(internal_write_ena[0]),
+	.data(internal_write_data), .wraddress(internal_write_address),
+	.q(internal_read_data[0]), .rdaddress(read_add)
+);
+ram ram1 (
+	.clock(clk),
+	.wren(internal_write_ena[1]),
+	.data(internal_write_data), .wraddress(internal_write_address),
+	.q(internal_read_data[1]), .rdaddress(read_add)
+);
+ram ram2 (
+	.clock(clk),
+	.wren(internal_write_ena[2]),
+	.data(internal_write_data), .wraddress(internal_write_address),
+	.q(internal_read_data[2]), .rdaddress(read_add)
+);
 
-ram ub3d0_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d0_internal_ram_write_enab[0]),
-   .q(ub3d0_internal_ram_data[0]), .rdaddress(ub3d0_ram_addr));
-ram ub3d0_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d0_internal_ram_write_enab[1]),
-   .q(ub3d0_internal_ram_data[1]), .rdaddress(ub3d0_ram_addr));
-ram ub3d0_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d0_internal_ram_write_enab[2]),
-   .q(ub3d0_internal_ram_data[2]), .rdaddress(ub3d0_ram_addr));
-ram ub3d1_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d1_internal_ram_write_enab[0]),
-   .q(ub3d1_internal_ram_data[0]), .rdaddress(ub3d1_ram_addr));
-ram ub3d1_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d1_internal_ram_write_enab[1]),
-   .q(ub3d1_internal_ram_data[1]), .rdaddress(ub3d1_ram_addr));
-ram ub3d1_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d1_internal_ram_write_enab[2]),
-   .q(ub3d1_internal_ram_data[2]), .rdaddress(ub3d1_ram_addr));
-ram ub3d2_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d2_internal_ram_write_enab[0]),
-   .q(ub3d2_internal_ram_data[0]), .rdaddress(ub3d2_ram_addr));
-ram ub3d2_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d2_internal_ram_write_enab[1]),
-   .q(ub3d2_internal_ram_data[1]), .rdaddress(ub3d2_ram_addr));
-ram ub3d2_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub3d2_internal_ram_write_enab[2]),
-   .q(ub3d2_internal_ram_data[2]), .rdaddress(ub3d2_ram_addr));
+// RAM address counter and write info latcher
+assign write_done = internal_write_addr == RAM_SIZE - 1;
+always_ff @(posedge clk or negedge nrst)
+	if (~nrst) begin
+		internal_write_addr <= 0;
+		internal_write_data <= 0;
+		for (int i = 0; i < 3; i++) internal_write_ena[i] <= 0;
+	end else begin
+		if (write_done)
+			internal_write_addr <= 0;
+		else if (write_enab)
+			internal_write_addr <= internal_write_addr + 1;
+		else
+			internal_write_addr <= internal_write_addr;
 
-ram ub4d0_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d0_internal_ram_write_enab[0]),
-   .q(ub4d0_internal_ram_data[0]), .rdaddress(ub4d0_ram_addr));
-ram ub4d0_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d0_internal_ram_write_enab[1]),
-   .q(ub4d0_internal_ram_data[1]), .rdaddress(ub4d0_ram_addr));
-ram ub4d0_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d0_internal_ram_write_enab[2]),
-   .q(ub4d0_internal_ram_data[2]), .rdaddress(ub4d0_ram_addr));
-ram ub4d1_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d1_internal_ram_write_enab[0]),
-   .q(ub4d1_internal_ram_data[0]), .rdaddress(ub4d1_ram_addr));
-ram ub4d1_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d1_internal_ram_write_enab[1]),
-   .q(ub4d1_internal_ram_data[1]), .rdaddress(ub4d1_ram_addr));
-ram ub4d1_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d1_internal_ram_write_enab[2]),
-   .q(ub4d1_internal_ram_data[2]), .rdaddress(ub4d1_ram_addr));
-ram ub4d2_ram0 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d2_internal_ram_write_enab[0]),
-   .q(ub4d2_internal_ram_data[0]), .rdaddress(ub4d2_ram_addr));
-ram ub4d2_ram1 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d2_internal_ram_write_enab[1]),
-   .q(ub4d2_internal_ram_data[1]), .rdaddress(ub4d2_ram_addr));
-ram ub4d2_ram2 (.clock(clk),
-   .data(write_data), .wraddress(write_addr), .wren(ub4d2_internal_ram_write_enab[2]),
-   .q(ub4d2_internal_ram_data[2]), .rdaddress(ub4d2_ram_addr));
+		internal_write_data <= write_data;
+		for (int i = 0; i < 3; i++) internal_write_ena[i] <= write_ram == i;
+	end
+
+// RAM switching logic
+logic old_read_done, old_write_done;
+always_ff @(posedge clk or negedge nrst)
+	if (~nrst) begin
+		write_ram <= 0;
+		read_ram <= 1;
+		free_ram <= 2;
+		old_read_done <= old_write_done <= 0;
+	end else begin
+		old_read_done <= read_done;
+		old_write_done <= write_done;
+
+		if (~old_read_done && read_done && ~old_write_done && write_done) begin
+			// When both finished at the same time, the free RAM is outdated.o
+			// Thus, we only need to swap the read and write RAM
+			read_ram <= write_ram;
+			write_ram <= read_ram;
+		end else if (~old_read_done && read_done) begin
+			// Swap free buffer and read buffer
+			read_ram <= free_ram;
+			free_ram <= read_ram;
+		end else if (~old_write_done && write_done) begin
+			// Swap free buffer and write buffer
+			write_ram <= free_ram;
+			free_ram <= write_ram;
+		end
+	end
 
 endmodule
