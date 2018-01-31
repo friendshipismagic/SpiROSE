c055c86ec1337e798bd17b3b39dbab9e01ba230d
alexis.bauvin <alexis.bauvin@telecom-paristech.fr>
2018-01-31 11:27:09 +0100

    rgb_logic: Generate usable coordinates

--- FPGA/src/rgb_logic.sv
+++ FPGA/src/rgb_logic.sv
@@ -7,9 +7,17 @@ module rgb_logic (
     input logic        hsync,
     input logic        vsync,
 
+    // Pixel output
     output logic [23:0] pixel_data,
     output logic [31:0] pixel_idx,
     output logic        pixel_valid,
+
+    // Coordinates of the pixels in a µblock
+    output logic  [3:0] pixel_x,
+    output logic  [4:0] pixel_y,
+    // Coordinates of the µblock
+    output logic  [2:0] block_x,
+    output logic  [1:0] block_y,
 
     // Signal sent by SPI to tell the RGB logic to mark incoming pixels as valid
     input logic         rgb_enable
@@ -31,6 +39,43 @@ always_ff @(posedge rgb_clk or negedge nrst)
         // Normal pixels
         if (vsync_r && vsync && hsync)
             pixel_idx <= pixel_idx + 1;
+    end
+
+// Coordinate counters
+always_ff @(posedge rgb_clk or negedge nrst)
+    if (~nrst) begin
+        pixel_x <= 0;
+        pixel_y <= 0;
+        block_x <= 0;
+        block_y <= 0;
+    end else begin
+        // Start of image
+        if (~vsync_r && vsync) begin
+            pixel_x <= 0;
+            pixel_y <= 0;
+            block_x <= 0;
+            block_y <= 0;
+        end else if (vsync && hsync) begin
+            pixel_x <= pixel_x + 1;
+
+            if (pixel_x == 7) begin
+                pixel_x <= 0;
+                block_x <= block_x + 1;
+
+                if (block_x == 4) begin
+                    block_x <= 0;
+                    pixel_y <= pixel_y + 1;
+
+                    if (pixel_y == 15) begin
+                        pixel_y <= 0;
+                        block_y <= block_y + 1;
+
+                        if (block_y == 3)
+                            block_y <= 0;
+                    end
+                end
+            end
+        end
     end
 
 // Pixel data latcher
