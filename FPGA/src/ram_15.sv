b681b8e8d1cea5868ecca4413e4154707e6b8dcc
Clément Decoodt <clement@decoodt.eu>
2018-01-31 12:59:39 +0100

    Bugfix: use pixel_data and ram addr

--- FPGA/src/ram_15.sv
+++ FPGA/src/ram_15.sv
@@ -21,20 +21,21 @@ module ram_15 (
 
 logic [14:0] drivers_SOF_array;
 assign drivers_SOF = drivers_SOF_array[0];
+// Exported for SPI debug
+wire [23:0] ram_read_data [14:0];
+wire [6:0] ram_read_addr [14:0];
 
 generate
    genvar ram_number;
    for(ram_number = 0; ram_number < 15; ram_number++) begin: for_ram
          wire write_enable = (ram_number == block_number) && block_write_enable;
-         wire ram_read_addr;
-         wire [23:0] ram_read_data;
          ram ram(
             .clock(clk),
             .data(ram_data),
-            .rdaddress(ram_read_addr),
-            .wraddress(ram_data),
+            .rdaddress(ram_read_addr[ram_number]),
+            .wraddress(pixel_number),
             .wren(write_enable),
-            .q(ram_read_data)
+            .q(ram_read_data[ram_number])
          );
 
          wire [383:0] framebuffer_data;
@@ -47,8 +48,8 @@ generate
             .EOC(EOC),
             .SOF(SOF),
             .driver_SOF(drivers_SOF_array[ram_number]),
-            .ram_addr(ram_read_addr),
-            .ram_data(ram_read_data),
+            .ram_addr(ram_read_addr[ram_number]),
+            .ram_data(ram_read_data[ram_number]),
             .EOR(EOR)
          );
 
