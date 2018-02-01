54af93162b73f4dd3494a859c52338dcc50bfe20
Vincent Charbonnieras <vincent.charbonnieras@telecom-paristech.fr>
2018-02-01 07:04:57 +0100

    FPGA/src/ram_15: Fix module signals

--- FPGA/src/ram_15.sv
+++ FPGA/src/ram_15.sv
@@ -27,8 +27,6 @@ logic [14:0] drivers_SOF_array;
 assign drivers_SOF = drivers_SOF_array[0];
 
 logic [4:0] slice_shifter;
-wire [7:0] write_idx = (wslice_cnt-slice_shifter) % 32 == 0 ? (wslice_cnt-slice_shifter)/32 : 10;
-wire [7:0] read_idx = (rslice_cnt/32);
 logic end_of_read, end_of_write;
 
 always_ff @(posedge clk or negedge nrst)
@@ -37,33 +35,40 @@ always_ff @(posedge clk or negedge nrst)
         end_of_read <= '0;
         end_of_write <= '0;
     end else begin
-        if(read_idx == 7) begin
+        if(rslice_cnt == 255) begin
             end_of_read <= '1;
         end
-        if(write_idx == 7) begin
+        if(wslice_cnt == 255) begin
             end_of_write <= '1;
         end
         if(end_of_read && end_of_write) begin
-            end_of_read <= '0;
-            end_of_write <= '0;
+            end_of_read   <= '0;
+            end_of_write  <= '0;
             slice_shifter <= slice_shifter + 1'b1;
         end
     end
 
+wire [2:0] write_idx = wslice_cnt[7 -: 3];
+wire [2:0] read_idx  = rslice_cnt[7 -: 3];
+wire slice_block_w_ok = (slice_shifter == wslice_cnt%32);
+wire slice_block_r_ok = (slice_shifter == rslice_cnt%32);
+
 generate
-genvar ram_number, slice;
+genvar ram_number, slice_block;
 for(ram_number = 0; ram_number < 15; ram_number++) begin: for_ram
     wire [23:0] ram_read_data [15:0];
     wire [6:0] ram_read_addr;
-    for(slice = 0; slice < 7; slice++) begin: for_slice
-        wire write_enable = (ram_number == block_number) && block_write_enable && write_idx == slice;
+    for(slice_block = 0; slice_block < 7; slice_block++) begin: for_slice
+        wire write_enable = (ram_number == block_number)
+                         && block_write_enable
+                         && (write_idx == slice_block && slice_block_w_ok);
         ram ram(
             .clock(clk),
             .data(ram_data),
             .rdaddress(ram_read_addr),
             .wraddress(pixel_number),
             .wren(write_enable),
-            .q(ram_read_data[slice])
+            .q(ram_read_data[slice_block])
         );
     end
 
@@ -77,10 +82,10 @@ for(ram_number = 0; ram_number < 15; ram_number++) begin: for_ram
         .nrst(nrst),
         .data_out(framebuffer_data),
         .EOC(EOC),
-        .SOF(SOF),
+        .SOF(SOF && slice_block_r_ok),
         .driver_SOF(drivers_SOF_array[ram_number]),
         .ram_addr(ram_read_addr),
-        .ram_data(ram_read_data[rslice_cnt/32]),
+        .ram_data(ram_read_data[read_idx]),
         .EOR(EOR)
     );
 
